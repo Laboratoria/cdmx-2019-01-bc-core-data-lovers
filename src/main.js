259d5a2a69cedcd2491d6effd20afbb4cc9ea1d8
jaurinu <jaurinu@gmail.com>
2019-01-31 14:27:17 -0600

    Estructuramos basicamente el Html y en main accedemos a la data e imprimimos nombre y tipo de pokemon en Html

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const data =  POKEMON.pokemon;
+
+const imprimirLista = document.getElementById('imprimir-lista');
+
+const print=(name,type) => {
+    let result = `<p>nombre: ${name} Tipo: ${type}</p>`
+    imprimirLista.insertAdjacentHTML("beforeend",result);
+    return result;
+}
+
+data.forEach(element => {
+    let name=element.name;
+    let type=element.type;
+    print(name,type);
+});
+
