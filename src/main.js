098834d4e7e1530eb4c332d74e3a07dd0e4500e7
lawrayette <milhadasdepapel@gmail.com>
2019-01-31 16:31:14 -0600

    subiendo css

---
+++ src/main.js
@@ -0,0 +1,19 @@
+function changeOptions() {
+    document.getElementById("start").style.display="none"
+    document.getElementById("options").style.display="block"
+}
+
+function changeType () {
+    document.getElementById("options").style.display="none"
+    document.getElementById("finalResult").style.display="block"
+}
+
+function changeWeaknesses () {
+    document.getElementById("options").style.display="none"
+    document.getElementById("finalResult").style.display="block"
+}
+
+function changeHome () {
+    document.getElementById("finalResult").style.display="none"
+    document.getElementById("options").style.display="block"
+}
