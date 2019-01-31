2f35ff7a3c70ac050e7dc7855b3780d9bc3163eb
AndreaMyshkn <atrkvsk@hotmail.com>
2019-01-31 15:13:53 -0600

    subiendo forEach

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const page4 = document.getElementById('page4');
 const page5 = document.getElementById('page5');
 const page6 = document.getElementById('page6');
 const page7 = document.getElementById('page7');
-
-
+const totalData= document.getElementById("total-data");
+const pokemonData = POKEMON.pokemon
 
 //Botón de entrada
 enterToPikapedia.addEventListener("click", () => {
@@ -18,3 +18,26 @@ enterToPikapedia.addEventListener("click", () => {
     page2.classList.add('hide');
     page1.classList.add('show');
   })
+
+  const print =(namePokemons, num, type)=>{
+    let result=`<p> ${namePokemons}</p>
+    <p>${num}</p>
+    <p>${type}</p>`;
+    totalData.insertAdjacentHTML("beforeend", result);
+  
+  }
+  
+ pokemonData.forEach(element =>{
+  let namePokemons= element.name;
+  let num =element.num;
+  let type = element.type;
+   print(namePokemons, num, type);
+   
+ });
+
+ 
+
+
+ /*const pokemons=pokemonData.map( pokemons=> `${pokemons.name} ${pokemons.num} ${pokemons.type} `);
+ print(pokemons)
+ console.log(pokemons);*/
