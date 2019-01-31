da761b91b426b09db74388ab6967441941abb17e
Pau-za <molo_pau@hotmail.com>
2019-01-31 11:55:21 -0600

    test filter debe ser función, pero no corre porque hubo errores en la instalación

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-//filtrar datos para usar los indicadores relacionados con la educación
-
-//open space john
+//filtrar datos para usar los indicadores relacionados con la educació
+const WORLDBANK = WORLDBANK;
 const dataMex = WORLDBANK.MEX.indicators;
-console.log(dataMex);
 
 //función para traer el elemento del html donde vamos a pintar la data
 const indicator = document.getElementById('indicator');
@@ -15,17 +13,21 @@ const print = (indicatorName) => {
 //imprime todo, pero "data" es un objeto, así que para iterarlo, hay que hacer un forIn
 
 //función de extraer elementos:
-dataMex.forEach(element => {
+ dataMex.forEach(element => {
   let indicatorName = element.indicatorName;
   print(indicatorName)
 });
+
+//función de eventos click en los indicadores
+//const indicatorClick = document. 
 
 //función que extrae data, e itera ese arreglo para imprimir el contenido en el otro HTML
 dataMex.forEach(element => {
   let year = element.data;
   let indicatorName = element.indicatorName;
   for (let data in year) {
-    console.log(indicatorName, `year.${data} = ${year[data]}`); 
+   // if ()
+    return indicatorName, `year.${data} = ${year[data]}`; 
   }
 }) 
 //imprime ya todos los valores de la data de todas las variables!!
