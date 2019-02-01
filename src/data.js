1fcdb19f261462fd6015cf1707fb77eddfebbb8a
lore0223 <loremma23@gmail.com>
2019-02-01 10:37:28 -0600

    subiendo un cambio en data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,7 @@
+// esta es una función de ejemplo
+// puedes ver como agregamos la función a nuestro objeto global window
 
-  
-
-
+const example = () => {
+  return 'example';
+}; 
+window.example = example;
