4fd40b6c68b064a89d6b769f80cc2df512b7d50a
analhi7 <analhi-e-m7@gmail.com>
2019-02-01 09:17:06 -0600

    Pruba funcionamiento de git y ramas

---
+++ src/main.js
@@ -0,0 +1,13 @@
+//Varable del objeto LOL
+/*const dataLol = LOL.data;
+console.log(dataLol);
+
+//funcion para traer el elemento del html donde vamos a pintar
+const personajes = document.getElementById("personajes");
+
+//función para imprimir elementos en el html
+const imprimir = (personajesName)=> {
+    let result= `<p>${personajesName}</p>`
+    personajes.insertAdjacentHTML("beforeend", result);
+}  */
+
