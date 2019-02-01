e035ae5244aae1369de13ef28e269dc5154eeb6c
eve2921 <eve2921@hotmail.com>
2019-02-01 13:17:23 -0600

    pinta en la pantalla los nombres de los campeones

--- src/main.js
+++ src/main.js
@@ -1,29 +1,70 @@
-const lol = LOL;
- //const dataLol = LOL.data;
- console.log(lol);
+//const lol = LOL;
+//Convierte la base de datos en una variable 
+
+const dataLol = LOL.data;
+
+const imprimirPersonajes = document.getElementById('imprimirPersonajes')
+//Funci{on para imprimir nombres
+
+let name ="";
+const print=(name)=>{
+    let result = `<p>${name}</p>`
+    imprimirPersonajes.insertAdjacentHTML("beforeend", result);}
+
+    for (const key in dataLol) {
+        if (dataLol.hasOwnProperty(key)) {
+          const element = dataLol[key];
+          let name = element.name;
+          let tag = element.tags[0];
+          let img = element.img;
+
+print (name);
+ console.log(name);}}
+
+
+
+
+
+
+/*console.log(lol);
  console.log(lol.data)
-
- /*const dataname = document.getElementById('dataname');
-
- const print = (championsNames) => {
-  let resultName = `<h2>${championsNames}<h2>`
-  dataname.insertAdjacentHTML("beforeend", resultName);
- }
+ console.log("++++++++++++++++++++++++")
 
 
 
-  /*lol.forEach(element =>{
-      let championsNames = element.data;
-      console.log(championsNames)
-  });
-
-
-
-  /*dataLol.forEach(element =>{
-      let name = element.data;
-      let championsNames = element.name;
-      for (let data in name){
-          return championsNames, `name.${data} = ${name[data]}`;
+ //
+const search = document.getElementById('search')
+const buscadora = () => {
+  console.log(search.value)
+  for (const key in dataLol) {
+    if (dataLol.hasOwnProperty(key)) {
+      const element = dataLol[key];
+      let name = element.name;
+      let tag = element.tags[0];
+      let img = element.img;
+  
+      if(search.value === 'A'){
+        console.log(name)
       }
+  
+  
+      //console.log(element.info.defense)
+      
+    }
+  }
+}
+search.addEventListener('keyup', buscadora)
+console.log(LOL.data.Aatrox)
+const dataLol = LOL.data
+for (const key in dataLol) {
+  if (dataLol.hasOwnProperty(key)) {
+    const element = dataLol[key];
+    let name = element.name;
+    let tag = element.tags[0];
+    let img = element.img;
+    if(search.value === 'A'){
+      console.log('encontrado')
+    }
+  }} */
 
-  })*/
+  
