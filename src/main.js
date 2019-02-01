cc18f8bd93d4b0afb0065489fab6923f8359ed83
analhi7 <analhi-e-m7@gmail.com>
2019-02-01 13:39:17 -0600

    Estos cambios no me sirven

--- src/main.js
+++ src/main.js
@@ -1,13 +1,3 @@
-//Varable del objeto LOL
-/*const dataLol = LOL.data;
-console.log(dataLol);
 
-//funcion para traer el elemento del html donde vamos a pintar
-const personajes = document.getElementById("personajes");
 
-//función para imprimir elementos en el html
-const imprimir = (personajesName)=> {
-    let result= `<p>${personajesName}</p>`
-    personajes.insertAdjacentHTML("beforeend", result);
-}  */
 
