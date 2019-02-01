5a7af64e47ee0fd8f3304ad43ca412af2cc75b13
UnaTal <taliagyg@gmail.com>
2019-02-01 08:52:57 -0600

    con CSS avanzado y classList hide

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-console.log(INJURIES);
+const principalpage = document.getElementById('principalpage');
+const pedestrianspage = document.getElementById('pedestrianspage');
+const pedalcyclistspage = document.getElementById('pedalcyclistspage');
+const busoccupantspage = document.getElementById('busoccupantspage');
+const caroccupantspag = document.getElementById('caroccupantspag');
+const motorcyclistspage = document.getElementById('motorcyclistspage');
 
+const pedestrian_data = () => {
+    page1.classList.add('hide');
+    page2.classList.remove('hide');
+  }
