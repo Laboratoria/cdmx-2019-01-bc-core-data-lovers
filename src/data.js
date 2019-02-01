54ffde7fdcbd88bf9cb315f8ab9d4c3586f7b963
Ariadna Gutierrez <ariadnagtzr@gmail.com>
2019-02-01 10:34:54 -0600

    cambios en diseño

--- src/data.js
+++ src/data.js
@@ -1,8 +1,29 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
+/*const example = () => {
   return 'example';
 };
 
-window.example = example;
+window.example = example;*/
+
+
+//Traer array
+
+const dataEducacion = WORLDBANK.MEX.indicators;
+console.log(dataEducacion)
+
+// Enlazar elementos del DOM
+const indicador = document.getElementById("indicador");
+
+// función para imprimir elementos en el html
+const print = (indicadorName) => {
+  let result = `<h5> ${indicadorName} </h5>`
+  indicador.insertAdjacentHTML("beforeend", result)
+};
+
+// extraer elementos
+dataEducacion.forEach (element => {
+  let indicadorName = element.indicatorName;
+  print(indicadorName)
+});
