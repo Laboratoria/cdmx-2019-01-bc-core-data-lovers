4d5ffefcfeb31d5a84cdd4768a84ffd81b7f3f66
AcheZeta <ache.mariana@gmail.com>
2019-02-01 14:14:23 -0600

    Imprimiendo  en el html dos constantes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-const data = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons}`);
+const total = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons}`);
 const date = INJURIES.map(INJURIES => `${INJURIES.Year}`);
 const moto = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Motorcyclists}`);
 const bike = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedalcyclists}`);
 const walk = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedestrians}`);
 const transit = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Transit_Total}`);
 
-console.log(data, date, moto, bike, walk, transit)
+console.log(walk)
+
+document.getElementById("root").innerHTML= `${date},${moto}`;
 
 //Total_Injured_Persons
 //Total_Injured_Persons_Motorcyclists
