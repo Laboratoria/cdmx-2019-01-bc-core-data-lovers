753d43775c0ea7b3ee8d928ecd86ab0a88332788
AndieCruz <anndiie06@gmail.com>
2019-02-01 21:48:03 -0600

    Se hizo la estructura HTML y se dio estilo en CSS

--- src/data.js
+++ src/data.js
@@ -1,11 +1,5 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-const data = INJURIES;
-data.forEach(function(element) {
-  console.log(data[23].Total_Injured_Persons_Motorcyclists);
-
-});
-
 const example = () => {
   return 'example';
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-const data = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Motorcyclists},${INJURIES.Year}`);
-
+const data = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons}`);
+const date = INJURIES.map(INJURIES => `${INJURIES.Year} ${INJURIES.Total_Injured_Persons}`);
+const moto = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Motorcyclists}`);
+const bike = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedalcyclists}`);
+const walk = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedestrians}`);
+const transit = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Transit_Total}`);
 
 data.forEach(function(element) {
   let totalheridos = data[28].Total_Injured_Persons;
   let año = data[28].Year;
 
 });
-console.log(data);
+console.log(date);
 
 //Total_Injured_Persons
 //Total_Injured_Persons_Motorcyclists
