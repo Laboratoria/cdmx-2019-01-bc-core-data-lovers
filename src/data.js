d07b01458e07d824fc9a4eef05a408640a4bf79e
lore0223 <loremma23@gmail.com>
2019-02-01 18:52:44 -0600

    metimos en un arreglo los nombres por que queremos ordenarlos alfabéticamente (aun no ordena)

--- src/data.js
+++ src/data.js
@@ -1,7 +1,4 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-}; 
-window.example = example;
+// window.example = example;
