acfe6ef64b0234b8af6fd327638f0b11e7b0d539
Diana <aries_di@hotmail.com>
2019-02-02 22:18:51 -0600

    Creación de eventos para botones, creación de transición de páginas, Funcionalidad del botón Filtrar por: acomodo de código

--- src/main.js
+++ src/main.js
@@ -1,20 +1,67 @@
+//Declaración de variables para manipular la data
 const pokemones = document.getElementById('pokemones');
 const dataPokemon = POKEMON.pokemon;
 
+//Declaración para el boton filtrar
+const btnFilterBy = document.getElementById('btnFilterBy');
+const filtered_out = document.getElementById('filtered_out');
+const theme = document.getElementById("theme");
 
-const toPrint = (namePokemon,imagePokemon,typePokemon,weaknessesPokemon) =>{
-    let visualPokemon = `<div class="divPokemon"><p id="name">${namePokemon}</p><br><img src="${imagePokemon}">
-    <p id="details">Tipo: ${typePokemon}<br>Debilidades: ${weaknessesPokemon}</p></div>`;
+//Declaración para el boton Inicio
+const btnStart = document.getElementById('btnStart');
+const welcome = document.getElementById('welcome');
+const toReturn = document.getElementById('toReturn');
+
+btnStart.addEventListener("click", () => {
+    welcome.classList.add('hide');
+    pokemones.classList.remove('hide');
+    theme.classList.remove('hide');
+    
+})
+
+//Función para filtrar
+btnFilterBy.addEventListener("click",() =>{
+    pokemones.classList.add('hide');
+    filtered_out.classList.remove('hide');
+});
+
+toReturn.addEventListener("click", () =>{
+    filtered_out.classList.add('hide');
+    pokemones.classList.remove('hide');
+})
+
+//Funcion que imprime los datos que son llamdos y la imagen de los pokemones
+const toPrint = (namePokemon,imagePokemon,typePokemon,weaknessesPokemon,candyPokemon,eggPokemon) =>{
+    let visualPokemon = `<div class="divPokemon"><p class="name">  ${namePokemon}</p><br><img src="${imagePokemon}">
+    <p class="details"><br>Type: ${typePokemon}<br>Weaknesses: ${weaknessesPokemon}<br>
+    Candy: ${candyPokemon} <br> ${eggPokemon}</p></div>`;
     pokemones.insertAdjacentHTML("beforeend",visualPokemon);    
 };
 
+//Por cada elemento del arreglo muetsra los datos llamados
 dataPokemon.forEach(element =>{
     let imagePokemon = element.img;
     let namePokemon = element.name;
     let typePokemon = element.type;
     let weaknessesPokemon = element.weaknesses;
+    let candyPokemon = element.candy;
+    let eggPokemon = element.egg;
     
-    toPrint(namePokemon,imagePokemon,typePokemon,weaknessesPokemon);
+    toPrint(namePokemon,imagePokemon,typePokemon,weaknessesPokemon, candyPokemon,eggPokemon);
+});
+
+const toPrintFilterOut = (namePokemon,typePokemon,imagePokemon) => {
+    let visualPokemon = `<section id="filtered_out" class="divPokemon"><p class="name">${namePokemon}</p><br>
+    <img src="${imagePokemon}"><br><p class="details">Type: ${typePokemon}</p></section>`;
+    filtered_out.insertAdjacentHTML("beforeend",visualPokemon); 
+}; 
+
+dataPokemon.forEach(element =>{
+    let namePokemon = element.name;
+    let imagePokemon = element.img;
+    let typePokemon = element.type;
+    
+    toPrintFilterOut(namePokemon,typePokemon,imagePokemon);
 });
 
 
