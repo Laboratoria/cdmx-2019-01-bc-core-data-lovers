7d7aaa7654ce64355098c9aec94186fa6f4d28ab
DanySquirrel <danys2809@gmail.com>
2019-02-04 16:53:21 -0600

    Indicador pais- CSS

--- src/data.js
+++ src/data.js
@@ -1,8 +1,46 @@
 // Manipulación de datos a través de arreglos y objetos. La idea de este archivo es contener toda la funcionalidad que corresponda a obtener, procesar y manipular datos.
 //En este archivo esperamos que implementes las funciones detalladas en la sección de Pruebas Unitarias.
 
-const  = () => {
-  return 'example';
-};
 
-window.worldbank = ;
+ //Elige nombre de indicadores por país
+
+/*
+const dataPer= WORLDBANK.PER.indicators;
+dataPer.forEach(element => {
+    let indicatorName=element.indicatorName;
+
+    console.log (indicatorName);
+
+
+})
+
+
+
+const dataMex= WORLDBANK.MEX.indicators;
+dataMex.forEach  (element => {
+    let indicatorName=element.indicatorName;
+
+    console.log (indicatorName);
+
+
+})
+
+const dataBra= WORLDBANK.BRA.indicators;
+dataBra.forEach  (element => {
+    let indicatorName=element.indicatorName;
+
+    console.log (indicatorName);
+
+
+})
+
+const dataChl= WORLDBANK.CHL.indicators;
+dataChl.forEach  (element => {
+    let indicatorName=element.indicatorName;
+
+    console.log (indicatorName);
+
+
+})
+*/
+
