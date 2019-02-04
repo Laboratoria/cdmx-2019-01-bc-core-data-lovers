d2e22ad04d356281a176cf580740d87548e0d10e
AcheZeta <ache.mariana@gmail.com>
2019-02-04 15:14:31 -0600

    CSS actualizado de px a %, muestra la data ordenada en html con adjacenthtml

--- src/main.js
+++ src/main.js
@@ -5,23 +5,24 @@ const injuriesby = INJURIES;
 //Constante del elemento root del HTML
 const totalsecc = document.getElementById("total")
 //Función para imprimir en html 
-const printpage = (year) => {
-  let result = `Año: ${year}<br>`
+const printpage = (year, total, moto, bike, walk, transit) => {
+  let result = `<b>Año:</b> <i>${year}</i> <br><b>total de heridos:</b> <i>${total};</i> <b>por Moto:</b> <i>${moto};</i> <b>por bici:</b> <i>${bike}</i>; <b>peatones:</b> <i>${walk}</i>; <b>autos:</b> <i>${transit};</i>  <br><br> `
   totalsecc.insertAdjacentHTML("beforeend", result);
 }
 //Función ForEach para recorrer elementos.
 injuriesby.forEach(element => {
-let total = element.Total_Injured_Persons;
-let moto = element.Total_Injured_Persons_Motorcyclists;
-let year = element.Year;
-let bike = element.Total_Injured_Persons_Pedalcyclists;
-let walk = element.Total_Injured_Persons_Pedestrians;
-let transit = element.Total_Injured_Persons_Transit_Total;
+  let year = element.Year;
+  let total = element.Total_Injured_Persons;
+  let moto = element.Total_Injured_Persons_Motorcyclists;
+  let bike = element.Total_Injured_Persons_Pedalcyclists;
+  let walk = element.Total_Injured_Persons_Pedestrians;
+  let transit = element.Total_Injured_Persons_Transit_Total;
+  
+//Le digo qué quiero que imprima. Deben ir en orden. 
+  printpage(year, total, moto, bike, walk, transit)
 
-printpage(year)
-//ConsoleLog si muestra todos los elementos console.log(total)
+  //ConsoleLog si muestra todos los elementos console.log(total)
 
 });
 // root.insertAdjacentHTML('beforeend', );
 //console.log(injuriesby);
-
