802de60c8a72c787632e37d25c02a3d79ba490d5
Da-ani <daniellesarer93@hotmail.com>
2019-02-04 15:45:39 -0600

    Se crearon botones, Se creó función para obtener nombre de usuario, se creo función para obtener data, se agregaron imagenes en una carpeta llamada imagenes que se usan en css, se aplicó función de None y Block

--- src/data.js
+++ src/data.js
@@ -1,26 +1,22 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
+window.pokemon = {
 
-//const example = () => {
-  //return 'example';
-//};
+    show:(data) => {
 
-//window.example = example;
-const ShowData = {
+        const imprimir = (nombre,imagen,id) =>{
 
-const pokemones = document.getElementById('pokemons');
-const dataPokemon = POKEMON.pokemon;
+        const pokemons = document.getElementById("pokemons");
+        let nombrePokemon = `<div class="divPokemon"><img src="${imagen}">${nombre}${id}</div>`;
+        pokemons.insertAdjacentHTML("beforeend",nombrePokemon);
 
-const imprimir = (nombre, imagen, id) => {
-   let nombrePokemon = `<div class="divPokemon"><img src="${imagen}">${nombre}${id}</div>`
-   pokemones.insertAdjacentHTML("beforeend", nombrePokemon);
- };
+    }
 
- dataPokemon.forEach(elemento =>{
-let nombre = elemento.name;
-let imagen = elemento.img
-let id = elemento.id
-imprimir(nombre,imagen,id);
+    data.forEach(elemento =>{
+        let nombre = elemento.name;
+        let imagen = elemento.img;
+        let id = elemento.id;
+        imprimir(nombre, imagen, id);
+})
 
- })
+    return imprimir;
+}
 }
