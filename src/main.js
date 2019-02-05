00216ba7ca364d0c8cd64c871021b97405076a5c
Brendss <brenda.ale_@hotmail.com>
2019-02-05 00:33:57 -0600

    Se corrigio el main de HTML

--- src/main.js
+++ src/main.js
@@ -9,29 +9,16 @@ const dataPer= WORLDBANK.PER.indicators;
 console.log(dataPer)
 
 //funcion para traer el elemento del HTML donde vamos a pintar los indicadores
-//const indicator= document.getElementById("indicator");
+const indicator= document.getElementById("indicator");
 //funcion para imprimir elementos en el HTML
-/*const printChl=(indicatorName)=> {
-    let result=`<h3>${indicatorName}</h3>`
+const printChl=(indicatorName)=> {
+    let result=`<p>${indicatorName}</p>`
     indicator.insertAdjacentHTML("beforeend",result);
     }
 dataChl.forEach(element =>{
     let indicatorName=element.indicatorName;
     printChl(indicatorName)
-});*/
-
-/*
-$(document).ready(function(){
-    $(".menu-icon").on("click",function(){
-        $("nav ul").toggleClass("showing");
-    });
 });
 
-$(window).on("scroll", function(){
-    if($(window).scrollTop()){
-        $("nav").addClass("black");
-    }
-    else{
-        $("nav").removeClass("black");
-    }
-})*/
+
+
