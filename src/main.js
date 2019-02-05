130847ce63e46e5a442ea904afb540e9a9d940ab
AdrianaPi <psic.adriana.a@gmail.com>
2019-02-04 23:11:53 -0600

    Ajustes al CSS

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    document.getElementById("page1").style.display = "none"
+    document.getElementById("page2").style.display = "block"
+   }
+   
+   function functionN() {
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("resultados").style.display = "block";
+   }
+   
+   function functionA() {
+    document.getElementById("page2").style.display = "none";
+    document.getElementById("resultados").style.display = "block";
+   }
