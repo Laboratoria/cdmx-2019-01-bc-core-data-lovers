c326f362f635ed8b9bbace02b0e9dec6078341a7
AdrianaPi <psic.adriana.a@gmail.com>
2019-02-05 01:31:17 -0600

    CSS

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@ function myFunction() {
     document.getElementById("page2").style.display = "block"
    }
    
-   function functionN() {
-    document.getElementById("page2").style.display = "none";
-    document.getElementById("resultados").style.display = "block";
-   }
-   
    function functionA() {
     document.getElementById("page2").style.display = "none";
-    document.getElementById("resultados").style.display = "block";
+    document.getElementById("results").style.display = "block";
    }
+
+const root = document.getElementById("root");
+const literacyOptions = document.getElementById("literacyOptions");
+const dataLiteracy = document.getElementById("dataLiteracy");
+
+
+/*const print = (window.literacy) => {
+    let result = `<h5>${window.literacy}</h5>`
+    dataLiteracy.insertAdjacentHTML("beforeend", result);
+
+}*/
+const newData = () => {
+    let options = literacyOptions.value;
+    let literacy = filtroLiteracy(select);
+    dataLiteracy = document.write("Los datos son:");
+}
