d4ae2f0e41575fca28d0f80d442bf019c884ea2b
JessFabis <46533184+JessFabis@users.noreply.github.com>
2019-02-05 01:45:36 -0600

    Update data.js
    arreglando el data para que muestre los pokemons

--- src/data.js
+++ src/data.js
@@ -33,16 +33,7 @@ dataPokemon.forEach(element => {
 /*En esta seccion estoy extrayendo datos especificos de la data*/
 const pokes = document.getElementById("probando");
 const datapokes = POKEMON.pokemon;
-const imprimir = (imagen, nombre2) => {
-    let nombrePokemon = `<p> <img src="${imagen}">${nombre2}
-    </p>`;
-    pokes.insertAdjacentHTML("beforeend",nombrePokemon);
-  };
-  datapokes.forEach(elemento => {
-    let imagen = elemento.img;
-    let nombre2 = elemento.name;
-    imprimir(imagen,nombre2);
-  });
+
 const imprimir = (imagen, nombre2, id, candy, altura, peso, debilidades) => {
   let nombrePokemon = `<p> <img src="${imagen}">Nombre:${nombre2}
   <br> Su Id es :${id}</br>
@@ -52,10 +43,7 @@ const imprimir = (imagen, nombre2, id, candy, altura, peso, debilidades) => {
   <br> debilidades :${debilidades}</br>
   </p>`;
   pokes.insertAdjacentHTML("beforeend",nombrePokemon);
-
-
-  pokes.insertAdjacentHTML("beforeend", nombrePokemon);
-
+ 
 };
 datapokes.forEach(elemento => {
   let imagen = elemento.img;
@@ -70,11 +58,6 @@ datapokes.forEach(elemento => {
 
 
 });
-
-});
-
-//let water = datapokes.filter(pokemonWater => elemento.type =="Water");
-//document.addEventListener("click",elemento());
 
 
 
