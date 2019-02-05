2521935959826d3e35017aeffc8589d4a06cd10b
AndieCruz <anndiie06@gmail.com>
2019-02-04 18:36:02 -0600

    Se puede imprimir la data en el .html

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const injuriesby = INJURIES;
 // ConsoleLog = Array(33) [ {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, … ]
 
 //Constante del elemento root del HTML
-const totalsecc = document.getElementById("total")
-//Función para imprimir en html 
+const totalsecc = document.getElementById("know");
+//Función para imprimir en html
 const printpage = (year, total, moto, bike, walk, transit) => {
   let result = `<b>Año:</b> <i>${year}</i> <br><b>total de heridos:</b> <i>${total};</i> <b>por Moto:</b> <i>${moto};</i> <b>por bici:</b> <i>${bike}</i>; <b>peatones:</b> <i>${walk}</i>; <b>autos:</b> <i>${transit};</i>  <br><br> `
   totalsecc.insertAdjacentHTML("beforeend", result);
@@ -18,29 +18,11 @@ injuriesby.forEach(element => {
   let bike = element.Total_Injured_Persons_Pedalcyclists;
   let walk = element.Total_Injured_Persons_Pedestrians;
   let transit = element.Total_Injured_Persons_Transit_Total;
-  
-//Le digo qué quiero que imprima. Deben ir en orden. 
+
+//Le digo qué quiero que imprima. Deben ir en orden.
   printpage(year, total, moto, bike, walk, transit)
 
-const data = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons}`);
-const date = INJURIES.map(INJURIES => `${INJURIES.Year} ${INJURIES.Total_Injured_Persons}`);
-const moto = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Motorcyclists}`);
-const bike = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedalcyclists}`);
-const walk = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Pedestrians}`);
-const transit = INJURIES.map(INJURIES => `${INJURIES.Total_Injured_Persons_Transit_Total}`);
 
   //ConsoleLog si muestra todos los elementos console.log(total)
 
 });
-
-// root.insertAdjacentHTML('beforeend', );
-//console.log(injuriesby);
-
-console.log(date);
-
-//Total_Injured_Persons
-//Total_Injured_Persons_Motorcyclists
-//Total_Injured_Persons_Pedalcyclists
-//Total_Injured_Persons_Pedestrians
-//Total_Injured_Persons_Transit_Total
-
