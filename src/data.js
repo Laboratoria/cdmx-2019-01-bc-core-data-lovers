0ecef904f5542477fa3fbddd1a4bb40b65bdff3f
AndieCruz <anndiie06@gmail.com>
2019-02-06 11:16:01 -0600

    Agregue nueva sección para que se refleje la data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,21 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-const example = () => {
-  return 'example';
-};
+//const example = () => {
+//  return 'example';
+//};
 
-window.example = example;
+//window.example = example;
+
+const injuriesby = INJURIES;
+injuriesby.forEach(element => {
+  let year = element.Year;
+  let total = element.Total_Injured_Persons;
+  let moto = element.Total_Injured_Persons_Motorcyclists;
+  let bike = element.Total_Injured_Persons_Pedalcyclists;
+  let walk = element.Total_Injured_Persons_Pedestrians;
+  let transit = element.Total_Injured_Persons_Transit_Total;
+  if(year === "2011-01-04"){
+    console.log(total,moto, bike, walk, transit);
+  }
+
+});
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 
-//Creamos las constantes que traeran la data
-const injuriesby = INJURIES;
-// ConsoleLog = Array(33) [ {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, … ]
-
 //Constante del elemento root del HTML
 const totalsecc = document.getElementById("know");
+let yearButton = document.getElementById("yearButton")
+let userButton = document.getElementById("user")
+let search = document.getElementById("search")
 //Función para imprimir en html
 const printpage = (year, total, moto, bike, walk, transit) => {
   let result = `<b>Año:</b> <i>${year}</i> <br><b>total de heridos:</b> <i>${total};</i> <b>por Moto:</b> <i>${moto};</i> <b>por bici:</b> <i>${bike}</i>; <b>peatones:</b> <i>${walk}</i>; <b>autos:</b> <i>${transit};</i>  <br><br> `
   totalsecc.insertAdjacentHTML("beforeend", result);
 }
+
+
 //Función ForEach para recorrer elementos.
 injuriesby.forEach(element => {
   let year = element.Year;
