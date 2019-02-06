003d34251a00bbd10a986c276e9886ca8dd79f5e
Champy13 <hgftdrd@hotmail.com>
2019-02-06 17:43:50 -0600

    Implemetacion funcion de filtrado,regresa arreglo nuevo

--- src/data.js
+++ src/data.js
@@ -1,77 +1,90 @@
-window.pokemon = {
+window.data = {
 
-    show:(data, type) => {
+//     show:(data, type) => {
 
-        const imprimir = (nombre,imagen,id) =>{
+//         const imprimir = (nombre,imagen,id) =>{
 
-        const pokemons = document.getElementById("pokemons");
+//         const pokemons = document.getElementById("pokemons");
 
-        let nombrePokemon = `<div class="divPokemon"><figure class="imgPokemon"><img src="${imagen}"></figure>
-                            <p class="namePokemon"> ${nombre}</p><br><p class="idPokemon"> Número: ${id}</p></div>`;
+//         let nombrePokemon = `<div class="divPokemon"><figure class="imgPokemon"><img src="${imagen}"></figure>
+//                             <p class="namePokemon"> ${nombre}</p><br><p class="idPokemon"> Número: ${num}</p></div>`;
 
-        pokemons.insertAdjacentHTML("beforeend",nombrePokemon);
-
-
-
-        };
-    data.forEach(element =>{
-
-        let nombre = element.name;
-        let imagen = element.img;
-        let id = element.id;
-        imprimir(nombre, imagen, id);
-
-     })
-    return imprimir;
+//         pokemons.insertAdjacentHTML("beforeend",nombrePokemon);
 
 
 
-},
+//         };
+//     data.forEach(element =>{
 
-imprim: (tipo) => {
-  let arrNew = [];
+//         let nombre = element.name;
+//         let imagen = element.img;
+//         let num = element.num;
+//         imprimir(nombre, imagen, num);
 
-
-tipo.forEach(element => {
-  if(element.type[0] === 'Water' || element.type[1] === 'Water' || element.type[2] === 'Water'){
-    arrNew.push(element)
-  }
-
-
-
-})
-
-console.log(arrNew)
-return arrNew
-},
-
-impri: (tipo) => {
-  let arrNew = [];
-
-tipo.forEach(element => {
-  if(element.type[0] === 'Fire' || element.type[1] === 'Fire' || element.type[2] === 'Fire'){
-    arrNew.push(element)
-  }
-
-})
+//      })
+//     return imprimir;
 
 
-return arrNew
 
-},
+// },
 
-impr: (tipo) => {
-  const arrNew = [];
+filterPokemon:(dataPokemon,types) =>{
 
-tipo.forEach(element => {
-  if(element.type[0] === 'Grass' || element.type[1] === 'Grass' || element.type[2] === 'Grass'){
-    arrNew.push(element)
-  }
-
-})
-
-
-return arrNew
+    const arrayPokemon = dataPokemon;
+    const arrayFilter = arrayPokemon.filter(dataPokemon => dataPokemon.type.includes(types));
+    
+    return arrayFilter;
 
 }
+
+    
+
 }
+
+// imprim: (tipo) => {
+//   let arrNew = [];
+
+
+// tipo.forEach(element => {
+//   if(element.type[0] === 'Water' || element.type[1] === 'Water' || element.type[2] === 'Water'){
+//     arrNew.push(element)
+//   }
+
+
+
+// })
+
+// console.log(arrNew)
+// return arrNew
+// },
+
+// impri: (tipo) => {
+//   let arrNew = [];
+
+// tipo.forEach(element => {
+//   if(element.type[0] === 'Fire' || element.type[1] === 'Fire' || element.type[2] === 'Fire'){
+//     arrNew.push(element)
+//   }
+
+// })
+
+
+// return arrNew
+
+// },
+
+// impr: (tipo) => {
+//   const arrNew = [];
+
+// tipo.forEach(element => {
+//   if(element.type[0] === 'Grass' || element.type[1] === 'Grass' || element.type[2] === 'Grass'){
+//     arrNew.push(element)
+//   }
+
+// })
+
+
+// return arrNew
+
+// }
+// }
--- src/main.js
+++ src/main.js
@@ -1,59 +1,112 @@
+
 document.getElementById("next").addEventListener("click", ()=>{
   //se le marca a la compu que se le de click en el boton jugar para realizar la sig funcion Ya se ingreso el nombre (pablo) en el input-->
     let inputNombre = document.getElementById("inputName").value; //el valor (pablo) se va a guardar en mi variable inputNombre
-    sessionStorage.setItem("Nombre", inputNombre);
+    sessionStorage.setItem("Nombre", inputNombre); 
     let nom = sessionStorage.getItem("Nombre");//GET: estpy obteniendo el valor que guarde en set bajo la referencia nombre
     document.getElementById("NombreDeUsuario").innerHTML = nom;
     document.getElementById("getName").style.display="none";
     document.getElementById("aparicion").style.display="block";
 })
 
+const dataPokemon = POKEMON.pokemon;
+const buttonTypes = Array.from( document.getElementsByClassName("typesPokemon"));
 
-document.getElementById("showPokemons").addEventListener("click", ()=> {
-    let dataPokemon = POKEMON.pokemon;
-    window.pokemon.show(dataPokemon);
-    document.getElementById("aparicion").style.display="none";
-  })
+const obtenerTipo = (arregloNuevo) => {
+
+  arregloNuevo.map((tipoPokemon) => {
+      
+      tipoPokemon.addEventListener("click", (event) =>{
+      const getTypePokemon = event.target.id;
+      
+      const arregloFiltrado = window.data.filterPokemon(dataPokemon,getTypePokemon);
+      console.log(arregloFiltrado); 
+    })
+  });
+
+} 
+
+obtenerTipo(buttonTypes);
 
 
 
-document.getElementById("agua").addEventListener("click", () => {
-  const agua = document.getElementById("water");
-let typePokemon = POKEMON.pokemon;
-// const result = window.pokemon.show(typePokemon);
-const pokemonAgua = window.pokemon.imprim(typePokemon);
-pokemonAgua.forEach( element => {
-  let tipoPokemon = `<div><p class="tipooPokemon"> ${element.type}</p></div>
-  <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
-  agua.insertAdjacentHTML("beforeend", tipoPokemon);
 
-})
-})
 
-document.getElementById("fire").addEventListener("click", () => {
 
-  const fuego = document.getElementById("water");
-let typePoke = POKEMON.pokemon;
-// const result = window.pokemon.show(typePokemon);
-const pokemonFuego = window.pokemon.impri(typePoke);
-pokemonFuego.forEach( element => {
-  let tipoPoke = `<div><p class="tipooPokemon"> ${element.type}</p></div>
-  <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
-  fuego.insertAdjacentHTML("beforeend", tipoPoke);
 
-})
-})
 
-document.getElementById("grass").addEventListener("click", () => {
 
-  const grass = document.getElementById("water");
-let typePoke = POKEMON.pokemon;
-// const result = window.pokemon.show(typePokemon);
-const pokemonFuego = window.pokemon.impr(typePoke);
-pokemonFuego.forEach( element => {
-  let tipoPoke = `<div><p class="tipooPokemon"> ${element.type}</p></div>
-  <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
-  grass.insertAdjacentHTML("beforeend", tipoPoke);
 
-})
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("showPokemons").addEventListener("click", ()=> {
+//     window.pokemon.show(dataPokemon);
+//     document.getElementById("aparicion").style.display="none";
+//   })
+
+
+
+
+
+// document.getElementById("agua").addEventListener("click", () => {
+//   const agua = document.getElementById("water");
+// let typePokemon = POKEMON.pokemon;
+// // const result = window.pokemon.show(typePokemon);
+// const pokemonAgua = window.pokemon.imprim(typePokemon);
+// pokemonAgua.forEach( element => {
+//   let tipoPokemon = `<div><p class="tipooPokemon"> ${element.type}</p></div>
+//   <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
+//   agua.insertAdjacentHTML("beforeend", tipoPokemon);
+
+// })
+// })
+
+// document.getElementById("fire").addEventListener("click", () => {
+
+//   const fuego = document.getElementById("water");
+// let typePoke = POKEMON.pokemon;
+// // const result = window.pokemon.show(typePokemon);
+// const pokemonFuego = window.pokemon.impri(typePoke);
+// pokemonFuego.forEach( element => {
+//   let tipoPoke = `<div><p class="tipooPokemon"> ${element.type}</p></div>
+//   <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
+//   fuego.insertAdjacentHTML("beforeend", tipoPoke);
+
+// })
+// })
+
+// document.getElementById("grass").addEventListener("click", () => {
+
+//   const grass = document.getElementById("water");
+// let typePoke = POKEMON.pokemon;
+// // const result = window.pokemon.show(typePokemon);
+// const pokemonFuego = window.pokemon.impr(typePoke);
+// pokemonFuego.forEach( element => {
+//   let tipoPoke = `<div><p class="tipooPokemon"> ${element.type}</p></div>
+//   <div class="divPokemon"><figure class="imgPokemon"><img src="${element.img}"></figure>`;
+//   grass.insertAdjacentHTML("beforeend", tipoPoke);
+
+// })
+// })
