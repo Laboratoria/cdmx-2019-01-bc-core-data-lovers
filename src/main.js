f08c82965f4eb155fd325d2bb6845e3bde728676
DianaLiz11 <dianamunoz86@live.com.mx>
2019-02-06 14:29:45 -0600

    se agregan los botones para el filtro de todos los tipos de pokemon funcionando

--- src/main.js
+++ src/main.js
@@ -9,15 +9,13 @@ const allData = window.showAllData();
 const dataByType = window.filterByType('Poison');
 //console.log(dataByType);
 const showList = (pokemonList) => {
+  pokemones.innerHTML="";
   pokemonList.forEach(element => {
     let name = element.name;
     let image = element.img;
     printPokemon(name, image);
   });
 };
-
-const buttonWater = document.getElementById('button-water');
-const buttonBug = document.getElementById('button-bug');
 
 // Mike estuvo aquí
 const buttonsCollection = document.getElementsByClassName("typesPokemon");
@@ -30,16 +28,14 @@ const getTypePokemon = (arrayButtons) => {
 
       arrayButtons.map(boton => {
         boton.addEventListener("click",(event) =>{
-          console.log(event.target.name);
-
-          console.log(window.filterByType(event.target.name));
+          //console.log(event.target.name);
+          //console.log(window.filterByType(event.target.name));
           // Invocar una función que pinte Pokemon
-
-          showList(window.filterByType(event.target.name))
-
+          showList(window.filterByType(event.target.name));
         })
       })
 }
+
 getTypePokemon(buttonsArray)
 
 // INvoco una función que cuando le ponga un tipo de Pokemon me retorne un arreglo de Pokemons filtrados por ese tipo.
@@ -48,21 +44,14 @@ getTypePokemon(buttonsArray)
 
 //esta parte obtiene la ubicacion de la pagina y dependiendo de ésta ejecuta las funciones
 let ubication = location.href;
-console.log(ubication);
+
 if (ubication.includes('index.html')) {
 
 }
 else if (ubication.includes('typePokemon.html')) {
  // En esta le paso allData para mostrar toda la info o dataByType para mostrar la info filtrada
   //showList(allData);
-  buttonWater.addEventListener('click', () => {
-    pokemones.innerHTML="";
-    showList(window.filterByType('Water'));
-  });
-  buttonBug.addEventListener('click', () => {
-    pokemones.innerHTML="";
-    showList(window.filterByType('Bug'));
-  });
+
 }
 
 
