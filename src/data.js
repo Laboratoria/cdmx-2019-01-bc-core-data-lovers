1d60492f0f2334ed093194115273540d2875bf43
maridiaz <maritzadiaz92@gmail.com>
2019-02-06 17:47:08 -0600

    formato a data js y main js (se acomodó el código)

--- src/data.js
+++ src/data.js
@@ -1,42 +1,43 @@
 window.lol = {
   showData: (lolData) => {
-  for (const key in lolData) {
-    if (lolData.hasOwnProperty(key)) {
-      const element = lolData[key];
-      let img = element.splash;
-      let name = element.name;
-      let tags = element.tags;
-      let tags2 = element.tags;
-      if (typeof tags2[1] == "undefined"){
-        tags2[1]="";
+    for (const key in lolData) {
+      if (lolData.hasOwnProperty(key)) {
+        const element = lolData[key];
+        let img = element.splash;
+        let name = element.name;
+        let tags = element.tags;
+        let tags2 = element.tags;
+        if (typeof tags2[1] == "undefined") {
+          tags2[1] = "";
+        }
+        print(img, name, tags[0], tags2[1]);
       }
-      print(img, name, tags[0], tags2[1]);
+    }
+  },
+  searchData: (rol, arrayData) => {
+    for (let i = 0; i < rol.length; i++) {
+      rol[i].addEventListener("click", () => {
+        let rolId = rol[i].id;
+
+        let newArray = [];
+        arrayData.forEach(element => {
+          let img = element.splash;
+          let name = element.name;
+          const tags = element.tags[0];
+          const tags2 = element.tags[1];
+          if (tags === rolId) {
+            newArray.push(element);
+            print(img, name, tags, tags2);
+          }
+          if (tags2 === rolId) {
+            newArray.push(element);
+            print(img, name, tags, tags2);
+          }
+        });
+      })
     }
   }
-},
-searchData: (rol, arrayData)=> {
-  for( let i= 0; i<rol.length; i++){
-     rol[i].addEventListener("click",() => {
-     let rolId = rol[i].id;
-
-      let newArray = [];
-      arrayData.forEach(element => {
-      let img = element.splash;
-      let name = element.name;
-      const tags = element.tags[0];
-      const tags2 = element.tags[1];
-       if(tags === rolId){
-         newArray.push(element);
-         print(img, name, tags, tags2);
-     }
-      if (tags2 === rolId){
-        newArray.push(element);
-        print(img, name, tags, tags2);
-      }
-  });
-  })}
-}
-//esta llave cierra window.lol
+  //esta llave cierra window.lol
 }
 
 
