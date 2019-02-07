26c487f2b2d43e5aef2e0a29855e25559732663b
Brendss <brenda.ale_@hotmail.com>
2019-02-07 14:56:22 -0600

    adjunto codigo de silvana y john

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const filter = () => {
-  return 'example';
+/*window.data = { //enlazando el js
+countryFilter: (dataWorldbank, countries)=>{ //se utilizan dos puntos por ser un objeto
+  const dataWorldbank= WORLDBANK;
+
+  const dataWorldbank = dataWorldbank;
+  console.log(dataWorldbank);
+}
+}*/
+
+window.dataBase = {
+  dataFunction: (data) => {
+    let mayus = data.WORLDBANK.PER.indicatorName;
+    console.log(mayus)
+    let pal = '';
+    for(let i = 0; i< mayus.length; i++){
+      pal += mayus[i].toUpperCase()
+    }
+    return pal
+  }
 };
-
-window.filter = filter;
-
