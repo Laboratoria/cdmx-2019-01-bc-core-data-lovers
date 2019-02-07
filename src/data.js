34d25ad456572ba1239f093e0691f88113f2bda0
AndieCruz <anndiie06@gmail.com>
2019-02-07 15:48:37 -0600

    Nos regresa un solo objeto filtrando el año

--- src/data.js
+++ src/data.js
@@ -1,25 +1,38 @@
 window.data = {
 
-  consult: (yearbutton, userbutton) => {
-    const injuriesby = INJURIES;
+  consult: (yearValue, userValue) => {
+const injuriesby = INJURIES;
+    //traer data de injuries del año
+  //  console.log(injuriesby[1].Year);
 
-    injuriesby.forEach(element => {
-
-      let year = element.Year;
-      let total = element.Total_Injured_Persons;
-      let moto = element.Total_Injured_Persons_Motorcyclists;
-      let bike = element.Total_Injured_Persons_Pedalcyclists;
-      let walk = element.Total_Injured_Persons_Pedestrians;
-      let transit = element.Total_Injured_Persons_Transit_Total;
-      if (year === yearbutton) {};
+    let found = injuriesby.find(element => element.Year === "1965-01-04");
+console.log(found);
 
 
 
-      //Le digo qué quiero que imprima. Deben ir en orden.
-      printpage(year, total, moto, bike, walk, transit)
-      //ConsoleLog si muestra todos los elementos console.log(total)
-    });
 
-  }
 
+ injuriesby.forEach(element => {
+//
+//       let year = element.Year;
+//      //console.log(Object.keys(element));
+//     //  console.log(year);
+//       let total = element.Total_Injured_Persons;
+//       let moto = element.Total_Injured_Persons_Motorcyclists;
+//       let bike = element.Total_Injured_Persons_Pedalcyclists;
+//       let walk = element.Total_Injured_Persons_Pedestrians;
+//       let transit = element.Total_Injured_Persons_Transit_Total;
+//       if (year === yearValue && userValue === moto
+//         || userValue === bike || userValue === walk || userValue === transit ||
+//         userValue === total) {
+//       };
+// //console.log(yearValue, userValue);
+});
+// //console.log(yearValue, userValue);
+ }
+//
 }
+
+//Le digo qué quiero que imprima. Deben ir en orden.
+//  printpage(year, total, moto, bike, walk, transit)
+//ConsoleLog si muestra todos los elementos console.log(total)
