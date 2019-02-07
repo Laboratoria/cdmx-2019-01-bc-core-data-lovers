d1f0beafee5fc115c6074657c2c60aa5da7e99d5
AcheZeta <ache.mariana@gmail.com>
2019-02-07 16:45:31 -0600

    Buscando dentro de un objeto con .find

--- src/data.js
+++ src/data.js
@@ -2,20 +2,19 @@ window.data = {
 
   consultYear: (yearValue, userValue) => {
     const injuriesby = INJURIES;
-    injuriesby.forEach(element => {
+    let found = injuriesby.find(element => element.Year == yearValue)
+    console.log(found)
+    
+      //injuriesby.forEach(element => {
       //console.log(Object.keys(element))
-      let year = element.Year;
-      let total = element.Total_Injured_Persons;
-      let moto = element.Total_Injured_Persons_Motorcyclists;
-      let bike = element.Total_Injured_Persons_Pedalcyclists;
-      let walk = element.Total_Injured_Persons_Pedestrians;
-      let transit = element.Total_Injured_Persons_Transit_Total;
-      if (year == yearValue) {
-        resultesecc.innerHTML = `<b>Durante</b> <i>${year.substr(0,4)}</i> <br><b>Total de heridos en ese año fue:</b> <i>${total}; <b>en moto:</b> ${moto}, <b>en bici:</b> ${bike}, <b>en transito:</b> ${transit}</i>`
-      };
-      console.log(year)
-      //ConsoleLog si muestra todos los elementos console.log(total)
-    });
+      //let year = element.Year;
+      //let total = element.Total_Injured_Persons;
+      //let moto = element.Total_Injured_Persons_Motorcyclists;
+      //let bike = element.Total_Injured_Persons_Pedalcyclists;
+      //let walk = element.Total_Injured_Persons_Pedestrians;
+      //let transit = element.Total_Injured_Persons_Transit_Total;
 
-  }
-}
+      //ConsoleLog si muestra todos los elementos console.log(total)
+    }
+
+  };
