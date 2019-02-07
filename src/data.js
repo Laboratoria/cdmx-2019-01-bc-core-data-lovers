aa7da30ba7611a07133d5c204f222de33ca224f5
AndieCruz <anndiie06@gmail.com>
2019-02-07 16:33:27 -0600

    Filtra por año que le ingresa el usuario y lo muestra en consola

--- src/data.js
+++ src/data.js
@@ -5,10 +5,25 @@ const injuriesby = INJURIES;
     //traer data de injuries del año
   //  console.log(injuriesby[1].Year);
 
-    let found = injuriesby.find(element => element.Year === "1965-01-04");
-console.log(found);
+    let found = injuriesby.find(element => element.Year === yearValue);
+let result = found.Total_Injured_Persons;
+console.log(result);
 
- injuriesby.forEach(element => {
+// const obj = found.map(found => `${found.Year}`);
+// console.log(obj);
+
+//   let total = element.Total_Injured_Persons;
+//       let moto = element.Total_Injured_Persons_Motorcyclists;
+//       let bike = element.Total_Injured_Persons_Pedalcyclists;
+//       let walk = element.Total_Injured_Persons_Pedestrians;
+//      let transit = element.Total_Injured_Persons_Transit_Total;
+
+}
+
+
+
+};
+
 //
 //       let year = element.Year;
 //      //console.log(Object.keys(element));
@@ -23,11 +38,11 @@ console.log(found);
 //         userValue === total) {
 //       };
 // //console.log(yearValue, userValue);
-});
+//};
 // //console.log(yearValue, userValue);
- }
+ //}
 //
-}
+
 
 //Le digo qué quiero que imprima. Deben ir en orden.
 //  printpage(year, total, moto, bike, walk, transit)
