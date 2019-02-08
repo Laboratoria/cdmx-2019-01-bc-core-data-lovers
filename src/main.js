dd76337e8b3c46d3f20c5bb6f671939246a7cf74
JazminDominguez <j.dgez.p@gmail.com>
2019-02-08 16:03:19 -0600

    se ejecuta solo una función para imprimi, se borraron comentarios sobrantes, la función de filter ejecuta el filtrado por roles

--- src/main.js
+++ src/main.js
@@ -1,54 +1,32 @@
 
-/* const search = document.getElementById('search')
-const buscadora = () => {
-  console.log(search.value)
-  for (const key in dataLol) {
-    if (dataLol.hasOwnProperty(key)) {
-      const element = dataLol[key];
-      let name = element.name;
-      let tag = element.tags[0];
-      let img = element.img;
-  
-      if(search.value === 'A'){
-        console.log(name)
-      }
-  
-  
-      //console.log(element.info.defense)
-      
-    }
-  }
-}
-search.addEventListener('keyup', buscadora)
-console.log(LOL.data.Aatrox)
-const dataLol = LOL.data
-for (const key in dataLol) {
-  if (dataLol.hasOwnProperty(key)) {
-    const element = dataLol[key];
-    let name = element.name;
-    let tag = element.tags[0];
-    let img = element.img;
-    if(search.value === 'A'){
-      console.log('encontrado')32-8
-    } */
 
     const search = document.getElementById('search')
+
     /* Convierte la base de datos en una variable*/
     const dataLol = LOL.data;
-    /*trae el div de nombre "es" para identificarlo e imprimirlo ahí*/
-    const es = document.getElementById('es')
 
-    /* Variales botones*/
+    /*trae el div de nombre "es" para identificarlo e imprimirlo ahí*/
+    const showFirstChampionList = document.getElementById('showFirstChampionList')
+
+ //Trae la class que contiene el rol de la lista del menú desplegable 
+    const rol = document.getElementsByClassName("rolPersonajes");
+
+  //almacena en variable los id del primer evento de cambio de pantalla, la interacción para abrir y cerrar el menú
     const inicio = document.getElementById('inicio')
     const menu = document.getElementById('menu')
     const cerrar = document.getElementById('cerrar')
-    
+  
+  //vuelve la base de datos a un arreglo
+ const dataToArray = Object.values(dataLol); 
+
   //Funcion de botones
+  //click que hace cambio de la primera pantalla a la lista de personajes
 
    inicio.onclick = ()=>{
      document.getElementById("primera-pantalla").style.display = 'none';
      document.getElementById("fondo-de-lista-de-campeones").style.display = 'block';};
-  
+
+     // función del menú desplegable donde se encuentran los roles para filtrar  
    menu.onclick = ()=> {
       document.getElementById("mySidenav").style.width = "250px";};
 
@@ -56,38 +34,41 @@ for (const key in dataLol) {
         document.getElementById("mySidenav").style.width = "0px";};
   
 
-// Pintar en HTML
 
+/* ----------------------------------------------*/
   
-  const rol = document.getElementsByClassName("rolPersonajes");
-  const result = window.lol.iterarData(dataLol)
+  //Trae el resultado de la función filtrado de data.js
+  const datos  = window.lol.iterarData(dataLol)
 
-  const print =(result) => {
-    result.forEach(champ => {
+// función que imprime la primera iteración que da la lista completa de campeones
+  const print = (datos) => {
+    showFirstChampionList.innerHTML = "";
+    datos.forEach(champ => {
     let nameList = `<div class="nameList"><img class="lolIcons" src="${champ.img}"><div id="letras"><h1 id= "nombre" >${champ.name}</h1> <p id="titulo">${champ.title}</p></div></div>`;
-    es.insertAdjacentHTML("beforeend",nameList);});}
+    showFirstChampionList.insertAdjacentHTML("beforeend",nameList);});}
 
-               print(result)
+               print(datos)
    
-   //este hay que subirlo porque es global 
- const dataToArray = Object.values(dataLol);     
-
+    
+//toma el id de las "a" del menu desplegable
+//las itera, a cada una le asigna un addEventListener
+//las prepara para recibir click
+//extrae el ID de cada opción
+//llama a la sección donde se va a imprimir
+//llama la función de filtro del window, busca el id seleccionado dentro de la data y forma un nuevo arreglo con la data filtrada
+//limpia la sección previa 
+//itera el arreglo y va acumulando los resultados dentro de divs de impresión
+const selectByRol = () => {
   for ( let i=0; i<rol.length; i++){
     rol[i].addEventListener('click', () => {
       let rolId = rol[i].id;
-      const printSection = document.getElementById("es");
+
       const arrayRolesFiltered = window.lol.filtroDataRoles(rolId, dataToArray);
+      print(arrayRolesFiltered);})}}
+    selectByRol();
+      
 
-      printSection.innerHTML = "";
-
-      arrayRolesFiltered.map((character) => {
-        printSection.innerHTML += `<div class="nameList"><img class="lolIcons" src="${character.img}"><div id="letras"><h1 id= "nombre" >${character.name}</h1> <p id="titulo">${character.title}</p></div></div>`;
-      })
-    })}
-    
-
-
-
+  
 
     
 
