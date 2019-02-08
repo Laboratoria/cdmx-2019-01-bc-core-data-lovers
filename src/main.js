d70f63523adf0c09e3f893965d86e311e1ed5b6b
elenore29 <elenore.nava@gmail.com>
2019-02-08 15:11:04 -0600

    Agregando elementos al objeto de la funcion showData

--- src/main.js
+++ src/main.js
@@ -38,6 +38,40 @@ const selectRol = () => {
 
 selectRol();
 
+
+
+// const orderByAttack = {lolData.info.attack};
+// numbers.sort(compare = (a, b)=>  {
+//   return a - b;
+// });
+// console.log(orderByAttack);
+//
+// orderByAttack();
+//
+//
+//
+//
+// var maxSpeed = {
+//     car: 300,
+//     bike: 60,
+//     motorbike: 200,
+//     airplane: 1000,
+//     helicopter: 400,
+//     rocket: 8 * 60 * 60
+// };
+//
+// var sortable = [];
+// for (const vehicle in maxSpeed) {
+//     sortable.push([vehicle, maxSpeed[vehicle]]);
+// }
+// sortable.sort(function(a, b) {
+//     return a[1] - b[1];
+// });
+
+//[["bike", 60], ["motorbike", 200], ["car", 300],
+
+//["helicopter", 400], ["airplane", 1000], ["rocket", 28800]]
+
 /*
 tank.addEventListener('click', () => {
   const tankFilter = window.lol.searchData(lolData);
