0b0809d7c0801daed231ff034899caae16a50b04
Andrea0611 <andrea.parquiroz@gmail.com>
2019-02-07 18:35:44 -0600

    pa poner al dia

--- src/main.js
+++ src/main.js
@@ -154,7 +154,7 @@ const typeIndicator = () => {
 
 for(let i = 0; i<elements.length; i++){
   elements[i].addEventListener('click', () => {
-    let valElement = elements[i].value
-    window.worldBank.filter(dataMex, valElement)
+    let valElement = elements[i].value;
+    window.worldBank.filter(dataMex, valElement);
   })
 }
