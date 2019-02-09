3136f821c0eb04c30157108108dec3df47cf63c7
emonroy58 <emgmonroy@gmail.com>
2019-02-08 19:21:38 -0600

    Subiendo Grafico

--- src/main.js
+++ src/main.js
@@ -9,6 +9,11 @@ const selOption = document.getElementById('filtrar-pais');
 const orderAscDat = document.getElementById('order');
 const nameIndicators= document.getElementById('filtrar-indicador');
 let contenido = document.getElementById('contenido');
+const btgrafica = document.getElementById('btgrafica');
+let ctx = document.getElementById("myChart").getContext("2d");//contenido grafica
+let valorAnioX=[];
+let valorPorcentajeY=[];
+let pais;
 let contador=0;
 
 const mostrarMenu = () =>{//Funcion mantiene activo o desactivo el menu, segun el valor del contador
@@ -53,7 +58,7 @@ const filtraIndPais = () =>{
     let select= selOption.value; //Select optiene el valor de cada value "MEX", "PER","BRA","CHL"
     let indicador = nameIndicators.value;
     //console.log(select);
-     let pais = window.worldBank.filtroPais(baseDatos,select,indicador); //Se llama la funcion window.filtroPais con el valor de selec
+     pais = window.worldBank.filtroPais(baseDatos,select,indicador); //Se llama la funcion window.filtroPais con el valor de selec
      recorrerObjeto (pais); 
 }
 
@@ -66,7 +71,19 @@ let respuesta="";
  for (let i in pais) //for iteracon cada elemento del objeto
   {
     respuesta+="<li>"+"<b>"+i+":  "+"</b>"+pais[i].toFixed(2)+"%"+"</li>"+"<br>"; //cada elemnto del objeto se muestra en li, usando toFixed para delimitar decimales
-  }
+ }
+
+for (let i in pais) {//recorre objeto para pasar el año a nuevo arrego de eje X
+    valorAnioX.push(i);
+ }
+  //graficaX(valorAnioX)//llama funcion para asignar valores al eje X de la grafica
+
+
+ for (let j in pais) {//recorre objeto para pasar el año a nuevo arrego de eje Y
+    valorPorcentajeY.push(pais[j]);
+ }
+ // graficaY(valorPorcentajeY)//llama funcion para asignar valores al eje Y de la grafica
+ 
   document.getElementById('contenido').innerHTML = respuesta;   
  return respuesta;
 }
@@ -88,3 +105,70 @@ const orderByAscent = () =>{
     prtOrder(orderObj)
 }
 orderAscDat.addEventListener('change',orderByAscent); //funcion recorre el objeto*/
+
+
+
+/*graficaX = (valorAnioX)=>{
+    console.log(myChart.config.data.labels +=valorAnioX)
+return myChart.config.data.labels=valorAnioX;
+           
+    //return myChart.config.data.labels=valorAnioX;;
+    }
+
+  graficaY = (valorPorcentajeY)=>{
+   /* for (let j in pais) {//recorre objeto para pasar el año a nuevo arrego de eje Y
+        valorPorcentajeY.push(pais[j]);
+    }
+    //console.log(myChart.config.data.datasets.data=valorPorcentajeY)
+     
+     return valorPorcentajeY;
+    
+  }    */ 
+
+
+console.log(valorAnioX);
+console.log(valorPorcentajeY);
+
+const VerGrafica=()=>{
+
+myChart= new Chart(ctx, { //funcion grafica
+    type: 'line',
+    data: {
+        labels: valorAnioX,
+        datasets: [{
+            label: 'Indicador Demografico (%)',
+            data: valorPorcentajeY,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+})
+
+}
+
+btgrafica.addEventListener('click',VerGrafica);
+  
