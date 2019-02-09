4ae47fb68933060f4c0f39adb21c552c8c8f310c
OlgaContreras911 <rockersita.911@gmail.com>
2019-02-09 10:27:42 -0600

    prueba con gráfica

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,5 +1,14 @@
 require('../src/data.js');
 
+describe('filtroPais', () => {
+  it('is a function', () => {
+    expect(typeof filtroPais).toBe('function');
+  });
+
+  it('returns `filtroPais`', () => {
+    expect(filtroPais()).toBe('filtroPais');
+  });
+});
 
 describe('example', () => {
   it('is a function', () => {
