a862b9ccbc543c47037afbb2d41317cbb0803056
eve2921 <eve2921@hotmail.com>
2019-02-10 12:23:42 -0600

    css funcionando

--- src/data.js
+++ src/data.js
@@ -1,28 +1,25 @@
 window.lol = {
- //Itera la data para ingresar a los datos que necesitamos y la devuelve en arreglo
- iterarData: (datalol) => {
-   
-  let datosArr = []
-  for (const key in datalol) {
-  if (datalol.hasOwnProperty(key)) {
-    const element = datalol[key];
-    let obj = {
-      name: element.name,
-      tag: element.tags[0],
-      splash: element.splash,
-      title: element.title,
+  iterarData: (datalol) => {
+    let datosArr = []
+    for (const key in datalol) {
+   if (datalol.hasOwnProperty(key)) {
+     const element = datalol[key];
+     let obj = {
+       name: element.name,
+       tag: element.tags[0],
+       splash: element.splash,
+       title: element.title,
+     }
+     datosArr.push(obj)
     }
-    datosArr.push(obj)
    }
-  }
-  return datosArr
- },
-
- //filtra los roles 
-filtroDataRoles: (rolId, datosArr ) =>{  
-     filtradoPorRoles = datosArr.filter(element => element.tag === rolId);
-
-      return filtradoPorRoles
-    },
+   return datosArr
+  },
  
-}
+ filtroDataRoles: (rolId, datosArr ) =>{  
+      filtradoPorRoles = datosArr.filter(element => element.tag === rolId);
+ 
+       return filtradoPorRoles
+     },
+  
+ }
--- src/main.js
+++ src/main.js
@@ -5,95 +5,20 @@ const imprimirRoles = document.getElementById("imprimirRoles")  /*trae el div im
 const mostrar = document.getElementById("mostrar")
 //const arrayData = Object.values(datalol)
 
-<<<<<<< HEAD
-for(let i = 0; i<rol.length; i++){
-    rol[i].addEventListener('click', () => {
-        let buscado = rol[i].id
-    })
-}
-console.log('funciona')
-
-const filtrarRol = () => {
-    let arr =[];
-    const filtrotank = Object.values(datalol)
-    filtrotank.forEach(element => {
-      const roles = element.tags[0];
-      if(roles === "Assassin"){
-        arr.push(element)
-      }
-      
-    });
-    console.log(arr)
-  }
-
-
-//const filtrarRol = () => {
-  //  for(let i = 0; i<rol.length; i++)
-
-
-/*const filtrarRol = () => {
-    const filtrotank = Object.values(datalol)
-    filtrotank.forEach(element => {
-        const roles = element.tags[0];
-        if(roles === "Tank"){
-            // console.log(element);
-
-        }
-        
-    });
-}*/
-=======
->>>>>>> upstream/master
-
-
-<<<<<<< HEAD
-const roles = document.getElementById("roles").value;
-//*trae el div imprimir roles para identificarlo e imprimir
-const imprimirRoles = document.getElementById("imprimirRoles")
-const mostrar = document.getElementById("mostrar")
-=======
->>>>>>> upstream/master
-
 mostrar.addEventListener("click",() =>{
     const menu = document.getElementById("menu");
     menu.classList.toggle("mostrar");
 });
 
-<<<<<<< HEAD
-
-//esta función imprimir los tags de los personales*/
-let tags = "";
-const print =(name,imga,title) =>{
-    let result = `<div class="cuadro"><img class="imagen" src="${imga}"> ${name} ${title}</div>`; 
-    imprimirRoles.insertAdjacentHTML("beforeend",result);
-}
-
-/*let img = "";
-const printimg = (img) =>{
-    let resultimg = `<div class=><img src="${img}"${name}</div>`;
-    imprimirRoles.insertAdjacentHTML("beforeend", resultimg);
-}*/
-
-for (const key in datalol) {
-    if (datalol.hasOwnProperty(key)) {
-      const element = datalol[key];
-      let name = element.name;
-      let tag = element.tags[0];
-      let imga = element.img;
-      let title = element.title;
-      let dato = [`${name}, ${tag}`]
-      print(name,imga,title);
-
-    }
-};
-=======
 // const result = window.lol.iterarData(datalol)
 // console.log(result)
 
 const print = (datosArr) =>{
     imprimirRoles.innerHTML = "";
     datosArr.forEach(champ => {
-let pintar = `<div class="cuadro"><img class="imagen" src="${champ.splash}"> <div id="subtitulo"><h1 id="nombre">${champ.name}</h1> <h2 id"titulo"> ${champ.title}</h2></div></div>`; 
+let pintar = `<div class="cuadro">
+<img class="imagen" src="${champ.splash}"> 
+<div id="subtitulo"><h2 id="nombre">${champ.name}</h2> <h1 id"titulo"> ${champ.title}</h1></div></div>`; 
 imprimirRoles.insertAdjacentHTML("beforeend", pintar);
  })
 
@@ -111,4 +36,3 @@ const selectRol = () =>{
   }
 }
 selectRol();
->>>>>>> upstream/master
