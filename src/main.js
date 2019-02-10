859999b8ff16800b366fdc63c42c22141e5f2c45
Abygail27 <abygaailrrocha@gmail.com>
2019-02-09 21:00:28 -0600

    creación de interfaz con secciones funcionales al evento de click en la nav-bar, funcionalidad de los indicadores

--- src/main.js
+++ src/main.js
@@ -1,83 +1,42 @@
+const botonInfo = document.getElementById('informate'); //boton para entrar a la pagína donde se generar la filtración y se mostra la data 
+const welcomePage = document.getElementById('welcomePage');
+const pageForData = documente.getElementById('info');
+const selectorPais = document.getElementById('selectElementId'); //constantes para seleccionar el país y se dispare el evento al seleccionar uno   
+const pagesjumps = document.getElementById('donations', 'commits' , 'impact')//constantes para brincar a la section que se utiliza como una pagina
 
-//boton para entrar a la pagína donde se generar la filtración y se mostra la data 
-const botonInfo = document.getElementById('botonInfo');
+  
+document.getElementById('informate').addEventListener ('click' , ( ) => { 
+welcomePage.style.display = 'none';
+pageForData.style.display = 'block';  
 
-//constantes de cada section para que se muestran y ocultan creando la simulación de pagínas
-const root = document.getElementById('root');
-const informate = document.getElementById('informate');
-
-//constantes para seleccionar el país y se dispare el evento al seleccionar uno   
-const selectorPais = document.getElementById('selectorPais');
-const selectMex = document.getElementById('selectorPais');
-const selectPeru = document.getElementById('selectorPais');
-const selectBrasil = document.getElementById('selectorPais');
-
-//const para cada pais en el selector 
-
-const dataMEX = WORLDBANK.MEX.indicators;
-const indicatorMex = document.getElementById("selectMex");
-
-const dataPER = WORLDBANK.PER.indicators;
-const indicatorPeru = document.getElementById("selectPeru");
-
-const dataBRA = WORLDBANK.BRA.indicators;
-const indicatorBrasil = document.getElementById("selectBrasil");
-
-//función que dispara el evento de mostrar y ocultar la pagina root a la pagina informate 
-botonInfo.addEventListener('click', () => {
-  root.classList.add('hideElement');
-  informate.classList.remove('hideElement');
-
-
-  //const print = (year) => {
-  //let resultMex = `<h4>${year}</h4>`
-  //indicator.insertAdjacentHTML("beforeend", resultMex);
-  //};
-
+jumpto(anchor)() => {
+window.location.href = "#"+anchor;
+}
 });
-//Impresión Database México
-selectMex.addEventListener('change', () => {
-  let enclick = selectMex.value;
-  if(enclick === 'MEX'){
-    dataMEX.forEach(element => {
-      // console.log(element.data)
-      });
-      // console.log(dataMEX[15].data)
-      let años = dataMEX[15].data
-      for (const año in años) {
-        if (años.hasOwnProperty(año)) {
-          let element = años[año];
-          element = parseFloat(element)
-          // console.log(  parseInt(element)) 
-          if(element > 0){
-            console.log(element)
-          }
+const dataGlobal = WORLDBANK.indicators;
+const GlobalIndicator = document.getElementById("IndicatorName ");
+
+
+countries.addEventListener('change', () => { //Impresión Database para cada país
+  let enclick = selectorPais.value;
+  if (enclick === 'countries') {
+    dataGlobal.forEach(element => {
+      
+    });
+    // console.log(dataMEX[15].data)
+    let años = dataGlobal[15].data
+    for (const año in años) {
+      if (años.hasOwnProperty(año)) {
+        let element = años[año];
+        element = parseFloat(element)
+        // console.log(  parseInt(element)) 
+        if (element > 0) {
+          console.log(element)
         }
       }
+    }
 
   }
 
-  //   // console.log(dataMEX);
-  //   if (dataMEX >= 1)
-  //     console.log(dataMEX);
 });
 
-//Impresión de Database Perú
-selectPeru.addEventListener('change', () => {
-  dataPER.forEach(function (dataPER) {
-    // console.log(dataPER);
-    if (dataPER >= 1)
-      console.log(dataPER);
-  });
-  
-})
-
-//Impresión de Database Brasil
-selectBrasil.addEventListener('change', () => {
-  dataBRA.forEach(function (dataBRA) {
-    // console.log(dataBRA);
-     if (dataBRA >= 1)
-    console.log(dataBRA);
-   
-  });
-})
