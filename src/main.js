8eaea15f58803921e5c2b9e430fec179addc26d3
Claudia Ferrusca Servin <46570837+claudiaferrusca@users.noreply.github.com>
2019-02-10 22:31:16 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 // Estoy creando la funcion de mi boton GO, para que al darle click me pinte el resultado del input en pantalla 2
 const btngetName = document.getElementById('next');
 const trainerName = document.getElementById('nombre');
@@ -40,4 +39,3 @@ nombre.addEventListener("click", ()=>{
   pantalla1.classList.add('hide');
   pantalla2.classList.remove('hide');
 })
->>>>>>> 087b6b4f9d17d9d9c7d5f1c22c7ec3600bf28782
