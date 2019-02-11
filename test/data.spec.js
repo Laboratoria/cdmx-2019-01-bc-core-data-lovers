60f01b3de5a9286ec677d7422b01b4620778eff0
OlgaContreras911 <rockersita.911@gmail.com>
2019-02-11 17:00:13 -0600

    pasa los primeros dos test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,20 @@
-/*import '../src/data.js';
+
+require ('../src/data');
+describe('es una función llamada filtroPaís', () => {
+  it('is a function', () => {
+    expect(typeof window.worldBank.filtroPais).toBe('function');
+  });
+
+  //it('returns `example`', () => {
+    //expect(example()).toBe('example');
+  //});
+});
+describe ('es una función llamada OrderByAsc', () => {
+  it ('is a function', () => { 
+expect(typeof window.worldBank.orderByAsc).toBe('function');
+  });
+});
+/*
 
 describe('filtroPais', () => {
   it('is a function', () => {
@@ -9,13 +25,16 @@ describe('filtroPais', () => {
     expect(filtroPais()).toBe('filtroPais');
   });
 });
+import '/src/data.js'
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('filtroPais', () => {
+  test('llama a la data de Mex', () => {
+    const nomIndicator = jest.fn();
+    filtroPais(nomIndicator, 'MEX', );
+    expect(nomIndicator).toHaveBeenCalled();
   });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
+});
+const orderByObject = require('./data');
+test('la lista contiene el año 2016', () => {
+  expect(orderByObject()).toContain('2016');
 });*/
