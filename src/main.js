9fa6931ab4305dd7fd5ae4d639f35eae1b4a42b1
Karen Ramírez Legorreta <karenramirezlegorreta2@gmail.com>
2019-02-11 11:36:03 -0600

    cambio de fuente en el header

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 // Estoy creando la funcion de mi boton GO, para que al darle click me pinte el resultado del input en pantalla 2
 const btngetName = document.getElementById('next');
 const trainerName = document.getElementById('nombre');
@@ -8,22 +6,23 @@ const sectionName = document.getElementById('pantalla1');
 
 btngetName.addEventListener ("click",() => {
 const name = trainerName.value;
-bienvenido.innerHTML = '¡Hola entrenador '  +  name  +' bienvenido';
+bienvenido.innerHTML = '¡Hola entrenador '  +  name  +' Bienvenido!';
 sectionName.style.display ="none";
 
-  })
+ })
 
 
 
 // Estoy dando la funcion a mi boton de que al darle click me esconda los elementos de la pantalla1
 document.getElementById("next").addEventListener("click", () =>{
 document.getElementById("getName").style.display="none";
+
 })
 
 // Estoy dando la funcionalidad a mi boton Pokemones ordenados, para que me pinte data
 document.getElementById("showPokemons").addEventListener("click", ()=> {
-    const dataPokemon = POKEMON.pokemon;
-    window.pokemon.show(dataPokemon);
+   const dataPokemon = POKEMON.pokemon;
+   window.pokemon.show(dataPokemon);
 })
 
 // Estoy dando funcionalidad a mi boton de Pekemones ordenados, que me esconda la pantalla, 1 a la 2.
@@ -37,7 +36,50 @@ const pantalla1=document.getElementById('pantalla1');
 const pantalla2=document.getElementById('pantalla2');
 
 nombre.addEventListener("click", ()=>{
-  pantalla1.classList.add('hide');
-  pantalla2.classList.remove('hide');
+ pantalla1.classList.add('hide');
+ pantalla2.classList.remove('hide');
 })
->>>>>>> 087b6b4f9d17d9d9c7d5f1c22c7ec3600bf28782
+
+
+
+const dataPokemon = (name,img, type, height, weight, candy, candy_count, egg, spawn_chance, avg_spawns,
+spawn_time, multipliers, weaknesses,) =>{
+ const pokemons = document.getElementById("pokemons");
+ let namePokemon = `<div class="container"> <h1> ${num} </h1> <h2> ${name} </h2>;
+ <div> <img = class "imageBox" src =${img}> </div>
+ constructor()
+ <p> TYPE: ${type} </p>
+ <p>HEIGHT:${height}</p>
+ <p>WEIGHT:${weight}</p>
+ <p>CANDY:${candy}</p>
+ <p>CANDY COUNT:$ {candy_count}</p>
+ <p>EGG: ${egg}</p>
+ <p>SPAWN CHANCE: $ {spawn_chance}</p>
+ <p>AVERANGE SPAWNS: $ {avg_spawns}</p>
+ <p>SPAWN TIME:${spawn_time}</p>
+ <p>MULTIPLIERS:${multipliers}</p>
+ <p>WEAKNESSES:${weaknesses}</p>`
+ pokemons.insertAdjacentHTML("beforeend",namePokemon);
+
+
+};
+
+
+data.forEach(element =>{
+ let num = element.num;
+ let name = element.name;
+ let img = element.img;
+ let type = element.type;
+ let height = element.height;
+ let weight = element.weight;
+ let candy = element.candy;
+ let candy_count= element.candy_count;
+ let egg = element.egg;
+ let spawn_chance = element.spawn_chance;
+ let avg_spawns = element.avg_spawns;
+ let spawn_time = element.spawn_time;
+ let multipliers = element.multipliers;
+ let weaknesses = element.weaknesses;
+ data(num, name, img, type, height, weight, candy, candy_count, egg, spawn_chance, avg_spawns, spawn_time, multipliers,
+  weaknesses, );
+})
