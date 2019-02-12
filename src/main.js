e02f16538520687a41771b287c867922f40fc9d1
Elena <hrndzelena@gmail.com>
2019-02-12 12:20:57 -0600

    TEST HTML CSS
    TEST
    write test
    HTML
    Change id for class when are used more than once
    CSS
    change the id for class

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
 // let buttonMotorcycle =  document.getElementById('motorcyclists-show');
 // let content = document.getElementById('content');
 
-
+// const dataToSearch = window.INJURIES;
 const bottonstByMode = Array.from(document.getElementsByClassName('button-transport'));
 const contentByMode = Array.from(document.getElementsByClassName('infoBody'));
 const tableByMode = Array.from(document.getElementsByClassName('dataTable'));
@@ -21,7 +21,6 @@ for (let i = 0; i < bottonstByMode.length; i++) {
     //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
     showThePanel();
     contentByMode[i].style.display = 'block';
-
     //Print in the information panel all the information
     printTheData(accidentsData, tableByMode[i], printOnce[i]);
     printOnce[i] = false;
@@ -38,7 +37,6 @@ function getNeededData(fieldToSearch) {
   return window.data.getData(fieldToSearch);
 
 }
-
 //(this is only used if we preffer to show the panel via javascrip and not in the css)
 function showThePanel() {
   //open the data Panel
@@ -55,78 +53,3 @@ function printDataInHTML(year, number, tableToPrint, canPrint) {
     tableToPrint.insertAdjacentHTML('beforeend', `<tr><td>${year}</td><td>${number}</td></tr>`);
   }
 }
-//Add functionality To the buttons
-// buttonPedestrians.addEventListener('click', function () {
-//   //get the needed dataTable
-//   const accidentsData = getNeededData(buttonPedestrians.name);
-//   //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
-//   showThePanel();
-//   //Print in the information pnale all the information
-//   printTheData(accidentsData);
-// });
-//
-// //Add functionality To the buttons
-// buttonPedalcyclists.addEventListener('click', function () {
-//   //get the needed dataTable
-//   const accidentsData = getNeededData(buttonPedalcyclists.name);
-//   //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
-//   showThePanel();
-//   //Print in the information pnale all the information
-//   printTheData(accidentsData);
-// });
-//
-// //Add functionality To the buttons
-// buttonBus_occupants.addEventListener('click', function () {
-//   //get the needed dataTable
-//   const accidentsData = getNeededData(buttonBus_occupants.name);
-//   //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
-//   showThePanel();
-//   //Print in the information pnale all the information
-//   printTheData(accidentsData);
-// });
-//
-// //Add functionality To the buttons
-// buttonCar_occupants.addEventListener('click', function () {
-//   //get the needed dataTable
-//   const accidentsData = getNeededData(buttonCar_occupants.name);
-//   //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
-//   showThePanel();
-//   //Print in the information pnale all the information
-//   printTheData(accidentsData);
-// });
-//
-// //Add functionality To the buttons
-// buttonMotorcycle.addEventListener('click', function () {
-//   //get the needed dataTable
-//   const accidentsData = getNeededData(buttonMotorcycle.name);
-//   //Display the information panel (this is only used if we preffer to show the panel via javascrip and not in the css)
-//   showThePanel();
-//   //Print in the information pnale all the information
-//   printTheData(accidentsData);
-// });
-
-
-/*openMenuButton.addEventListener('click', ()=>{
-  openNav();
-});*/
-// let accidentsData = window.data.getData('Total_Injured_Persons_Motorcyclists');
-//
-//
-// accidentsData.forEach(element => {
-//   printDataInHTML(element.year, element.number);
-// });
-
-
-//alert("hola mundo");
-//console.log(INJURIES);
-
-//console.log(INJURIES[0].Year);
-//pedalcyclistspage.addEventListener()
-
-//menu
-/*function openNav() {
-  document.getElementById("side_menu").style.width = "250px";
-}
-function closeNav() {
-  document.getElementById("side_menu").style.width = "0";
-}*/
