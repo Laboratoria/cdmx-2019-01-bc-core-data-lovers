87ef0624e725955c0c058662c7ce0df22b736a7f
doctoramalvada <mydemoni@hotmail.com>
2019-02-12 16:02:06 -0600

    ya pasa el primer test

--- test/data.spec.js
+++ test/data.spec.js
@@ -3,13 +3,13 @@ require ('../src/data.js');
 
 describe('window.WorldBank deberia ser un objeto', () => {
   it('is a object', () => {
-    expect(window.WorldBank).toBe('object');
+    expect(typeof WorldBank).toBe('object');
   });
 });
 
 //hasta aqui todo bien
-describe('filterCountry es una función', () => {
+describe('WorldBank should be a function', () => {
   it('is a function', () => {
-    expect(window.WorldBank.filterCountry).toBe('function');
+    expect(typeof window.WorldBank.filterCountry).toBe('function');
   })
 });
