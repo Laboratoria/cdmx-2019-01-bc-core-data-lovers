f08ad6e214f71fd60d348134935b04636344c839
OlgaContreras911 <rockersita.911@gmail.com>
2019-02-12 00:31:07 -0600

    tres test pasan limpio

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,26 +1,28 @@
 require ('../src/data');
-describe('es un objeto llamado WorldBank', () => {
-  it('is an object', () => {
-    expect(typeof window.worldBank).toBe('object');
+describe ('es un Objeto llamado WorldBank', () => {
+  it ('is an object', () => { 
+expect(typeof window.worldBank).toBe('object');
+  });
+});
+
+describe('es una función llamada filtroPaís', () => {
+  it('is a function', () => {
+    expect(typeof window.worldBank.filtroPais).toBe('function');
+  });
+
+  //it('returns `example`', () => {
+    //expect(example()).toBe('example');
+  //});
+});
+describe ('es una función llamada OrderByAsc', () => {
+  it ('is a function', () => { 
+expect(typeof window.worldBank.orderByAsc).toBe('function');
   });
 });
 describe('es una función llamada filtroPaís', () => {
   it('is a function', () => {
     expect(typeof window.worldBank.filtroPais).toBe('function');
   });
+
  
-
-  //it('returns `example`', () => {
-    //expect(example()).toBe('example');
-  //});
-});
-
-describe ('es una función llamada OrderByAsc', () => {
-  it ('is a function', () => { 
-expect(typeof window.worldBank.orderByAsc).toBe('function');
-  });
-});
-const orderByObject = require('./data');
-test('la lista contiene el año 2016', () => {
-  expect(orderByObject()).toContain('2016');
 });
