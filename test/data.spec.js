4a0bdf71e707b1566adfdcab0245058f8aacc875
elenore29 <elenore.nava@gmail.com>
2019-02-12 15:03:52 -0600

    Realizamos los test para saber que cada funcion debe ser una función y comenzamos el test para la función de filtrar pero lanza un error al correr el test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,46 @@
 require('../src/data.js');
+const newArrayInfo = window.lol.showData;
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('window.lol', () => {
+  it('is an object', () => {
+    expect(typeof window.lol).toBe('object');
   });
+});
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+//Test de la función para filtrar por rol
+describe('filterByRol', () => {
+  it('is a function', () => {
+    expect(typeof window.lol.filterByRol).toBe('function');
+  });
+  it('Should returns the object Aatrox if we select Tank', () => {
+    expect(window.lol.filterByRol(newArrayInfo, 'Tank')[0].toEqual(window.LOL.data.Aatrox.tags[1]));
+  });
+});
+
+// Test de la función para ordenar por ataque
+describe('sorterByAttack', () => {
+  it('is a function', () => {
+    expect(typeof window.lol.sorterByAttack).toBe('function');
+  });
+});
+
+// Test de la función para ordenar por magia
+describe('sorterByMagic', () => {
+  it('is a function', () => {
+    expect(typeof window.lol.sorterByMagic).toBe('function');
+  });
+});
+
+// Test de la función para ordenar por defensa
+describe('sorterByDefense', () => {
+  it('is a function', () => {
+    expect(typeof window.lol.sorterByDefense).toBe('function');
+  });
+});
+
+// Test de la función para buscar por nombre
+describe('filterByName', () => {
+  it('is a function', () => {
+    expect(typeof window.lol.filterByName).toBe('function');
   });
 });
