c7c139c90d249162b7627233be3b3b298af3e6cf
doctoramalvada <mydemoni@hotmail.com>
2019-02-12 15:12:56 -0600

    pasando test de objeto y funcion, falla 1

--- test/data.spec.js
+++ test/data.spec.js
@@ -6,3 +6,10 @@ describe('window.WorldBank deberia ser un objeto', () => {
     expect(window.WorldBank).toBe('object');
   });
 });
+
+//hasta aqui todo bien
+describe('filterCountry es una función', () => {
+  it('is a function', () => {
+    expect(window.WorldBank.filterCountry).toBe('function');
+  })
+});
