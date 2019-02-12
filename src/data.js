adaf0606852a3f986a6b87437376e40b1b938a87
lawrayette <milhadasdepapel@gmail.com>
2019-02-12 13:07:12 -0600

    función de filtrado  pura y jalando

--- src/data.js
+++ src/data.js
@@ -1,8 +1,14 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+
+
+window.allPokemon = { 
+  dataFiltered:(data,types) =>{
+  const arrayPokemon= data ;
+    const arrayFilter = arrayPokemon.filter(arrayPokemon => arrayPokemon.type.includes(types));
+    return arrayFilter;
+  }
+  
+  }
