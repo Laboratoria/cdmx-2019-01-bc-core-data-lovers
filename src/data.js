f83776ede4f534b24698356bc67ee1a509b94b57
Brendss <brenda.ale_@hotmail.com>
2019-02-13 10:16:41 -0600

    Se unifican todas las comillas simples a dobles y se agregan puntos y comas faltantes

--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 window.WorldBank = {
   dataCountries: (data, countries) => {
-  let dataByCountries = data[countries].indicators
+  let dataByCountries = data[countries].indicators;
   return dataByCountries;
   },
   percentAndYear:(data, matchYear) =>{
     let newData= {}
     data.forEach(objeto => {
       if(objeto.indicatorName === matchYear){
-        newData = objeto.data 
+        newData = objeto.data; 
         return newData
       }
     });
--- src/main.js
+++ src/main.js
@@ -1,45 +1,45 @@
 const dataWorldbank = WORLDBANK;
-const buttonCountries = document.getElementsByClassName('button');
-const selectIndicator = document.getElementById('indicator')
-const dataYear = document.getElementById('data-year')
+const buttonCountries = document.getElementsByClassName("button");
+const selectIndicator = document.getElementById("indicator");
+const dataYear = document.getElementById("data-year");
 
 
 const buttonClick = () => {
   let countries = event.target.id;
-  let result = window.WorldBank.dataCountries(dataWorldbank, countries)
-  printOption (result)
+  let result = window.WorldBank.dataCountries(dataWorldbank, countries);
+  printOption(result)
 }
 
-for(let i = 0; i< buttonCountries.length; i++){
-  buttonCountries[i].addEventListener('click', buttonClick)
+for (let i = 0; i < buttonCountries.length; i++) {
+  buttonCountries[i].addEventListener("click", buttonClick)
 }
 
 const printOption = (result) => {
   const dataFilter = result.filter(indicator => {
-    let printIndicatorName = indicator.indicatorName
+    let printIndicatorName = indicator.indicatorName;
     let education = /educación/i
-    let matchs = printIndicatorName.match(education)
-    if(matchs !== null){
-      let indicatorOption =`<option value="${printIndicatorName}" >${printIndicatorName}</option>`
-      selectIndicator.insertAdjacentHTML('beforeend', indicatorOption)
+    let matchs = printIndicatorName.match(education);
+    if (matchs !== null) {
+      let indicatorOption = `<option value="${printIndicatorName}" >${printIndicatorName}</option>`
+      selectIndicator.insertAdjacentHTML("beforeend", indicatorOption)
       return matchs
     }
   });
   selectIndicator.addEventListener('change', () => {
-    let matchYear = event.target.value
-    let resultYear = window.WorldBank.percentAndYear(dataFilter, matchYear)
-    printYears(resultYear);    
+    let matchYear = event.target.value;
+    let resultYear = window.WorldBank.percentAndYear(dataFilter, matchYear);
+    printYears(resultYear);
   })
 }
 
 const printYears = (resultYear) => {
   for (const key in resultYear) {
     if (resultYear.hasOwnProperty(key)) {
-      let element =resultYear[key];
-      const year=key;
-      if(element !== ""){
-        let indicatorDataYear=`<p>Año: <span>${year}   </span> <span>${parseFloat(element).toFixed(2)}%</span></p>`
-      dataYear.insertAdjacentHTML('beforeend', indicatorDataYear)  
+      let element = resultYear[key];
+      const year = key;
+      if (element !== "") {
+        let indicatorDataYear = `<p>Año: <span>${year}   </span> <span>${parseFloat(element).toFixed(2)}%</span></p>`
+        dataYear.insertAdjacentHTML('beforeend', indicatorDataYear)
       }
     }
   }
