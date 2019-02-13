0adb8d2cf10aa4ce4d1197b7f15d4e3f8d8712e2
AcheZeta <ache.mariana@gmail.com>
2019-02-13 12:00:37 -0600

    For each para mostrar todos los resultados de un usuario.

--- src/data.js
+++ src/data.js
@@ -6,11 +6,15 @@ window.data = {
     let userResult = "";
     //Usamos FIND para buscar el año dentro del objeto.
     const yearFound = injuriesBy.find(element => element.Year == yearValue);
+    const prueba = injuriesBy.forEach(element => {
+      console.log(element.Total_Injured_Persons_Motorcyclists)
+    })
+
     //Dentro de un ciclo FOR buscamos las Key y comparamos con el valor del Usuario.
     for (const key in yearFound) {
       if (yearFound.hasOwnProperty(key)) {
         const element = yearFound[key];
-        if (key == userValue) {
+          if (key == userValue) {
           yearResult = yearValue.substr(0, 4);
           userResult = element;
         } //for if If 
