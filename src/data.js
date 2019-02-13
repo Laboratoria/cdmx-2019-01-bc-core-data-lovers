3ce09052a3c0cf7110fb0885f6c2e797aebbb70c
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-02-13 15:12:42 -0600

    funcion de filtro testeada

--- src/data.js
+++ src/data.js
@@ -12,28 +12,30 @@
 //   }
 
 window.WorldBank = {
-  filterCountry: (data,listQuestion) =>{
-    let country = listQuestion.dataset.ciudad;// Obtenemos la ciudad de la que vamos a filtrar, es decir, obtenemos el data-ciudad del select
+  filterCountry: (data, country, countrySelect) =>{
+    //console.log(data,listQuestion)
+    //let country = listQuestion.dataset.ciudad;// Obtenemos la ciudad de la que vamos a filtrar, es decir, obtenemos el data-ciudad del select
+    // console.log(country)
     let paisElegido = data[country].indicators // entrams a la data de cada pais y los indicadores
-    let countrySelect = listQuestion.value; //obtenemos el valor que tiene de la pregunta
+    //et countrySelect = listQuestion.value; //obtenemos el idicator code que tiene de la pregunta
     let result = {}; //declaramos una variable vacia  para que aqui almacene el resultado
     // if(country === 'ciudadesMex'){ // comparamos la data-ciudad que sea igual a la que seleccionamos
       paisElegido.forEach(ciudad =>{ //  seleccionamos la idicadores de mexico 
         if(ciudad.indicatorCode == countrySelect){ //si el codigo y el select son iguales
+          //console.log(result)
           result = ciudad.data //retornamos la data años y numeros 
         }
       });
-    console.log(result)
+    //console.log(result)
     return result //retorna a la variable vacia que declaramos al inicio
   },
   // order numbers en mayor, menor, asc, desc
   orderData: (data, sortBy, sortOrder) =>{
     let orderDataYear = {}; // empty array
     let unorderDataYear = data; // object data
-
     switch(sortBy){
         case "years": { 
-          console.log("order by years");
+          //console.log("order by years");
           if(sortOrder === 'asc'){ 
             // menor a mayor
             orderDataYear = Object.keys(unorderDataYear).sort((a,b) => a - b)
@@ -42,95 +44,21 @@ window.WorldBank = {
             orderDataYear = Object.keys(unorderDataYear).sort((a,b) => b - a)
           }
           return orderDataYear;
-          break; 
+           
         }
         case "values": { 
-          console.log("order by values");
+          //console.log("order by values");
           if(sortOrder === 'asc'){  // menor a mayor
             orderDataYear = Object.keys(unorderDataYear).sort((a,b) => unorderDataYear[a] - unorderDataYear[b] )
           }
           else if(sortOrder === 'desc'){
-            console.log("desc");// mayor a menor
+            //console.log("desc");// mayor a menor
             orderDataYear = Object.keys(unorderDataYear).sort((a,b) => unorderDataYear[b] - unorderDataYear[a] )
           }
-          console.log(orderDataYear);
+          //console.log(orderDataYear);
           return orderDataYear;
           
-          break; 
         }
     }
-  }
-  
-
-
-  // orderData: (data, sortBy, sortOrder) => {
-  // let orderData = [];
-  // switch(sortBy){
-  //   case'years':
-  //     if(sortOrder === 'mayor'){
-  //       orderData = data.sort((a,b ) => (a.years > b.years)? 1 : -1);
-  //       console.log (orderData);
-  //     }
-  //     else if(sortOrder === 'menor'){
-  //       orderData = data.sort((a,b)=>(a.years > b.years)? -1 : 1);
-  //     }
-  //     // break;
-  //     case'numbers':
-  //     if(sortOrder === 'asc'){
-  //       orderData = data.sort((a,b ) => (a.num > b.num)? 1 : -1);
-  //     }
-  //     else if(sortOrder === 'desc'){
-  //       orderData = data.sort((a,b)=>(a.num> b.num)? -1 : 1);
-  //     }
-  //     break;
-  // }
-  // return orderData;
-
-  
-}
-
-function compareNumbers(a, b) {
-  return b-a;
-}
-  
-// sort 
-var pokemons = [
-  { name:'pickachu', level: 50 } , 
-  { name:'bulbasour', level: 10 } , 
-  { name:'snorlax', level: 1 } , 
-  { name:'mew', level: 2 } , 
-  { name:'abra', level: 150 } 
-];
-const countryM = WORLDBANK.MEX.indicators;
-var numbers = [2, 34, 10, 2, 5, 1];
-console.log(countryM.sort(function (prev, next) {
-  if (prev.name > next.name) {
-    return 1;
-  }
-  if (prev.name < next.name) {
-    return -1;
-  }
-  return 0;
-//  return prev.level - next.level;
-}));
-
-var WORLDBANK = {
-  "PER": {
-    "indicators": [
-      {
-        "data": {
-         
-          "2001": "",
-          "2002": 31.4799995422363,
-          "2003": 29.6299991607666,
-          "2004": 27.6299991607666,
-          "2005": 27.2099990844727,
-          "2006": 26.9699993133545,
-        },
-        "countryName": "Perú",
-        "countryCode": "PER",
-        "indicatorName": "Empleo de tiempo parcial, mujeres (% del total de mujeres empleadas)",
-        "indicatorCode": "SL.TLF.PART.FE.ZS"
-      }]
-  }
-}
+  },  
+ }
