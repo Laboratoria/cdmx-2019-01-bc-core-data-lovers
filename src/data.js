f77631b4917c7d6ba31352693616b65a81610666
JessFabis <46533184+JessFabis@users.noreply.github.com>
2019-02-14 09:48:25 -0600

    Arreglando Data

--- src/data.js
+++ src/data.js
@@ -54,7 +54,7 @@ sortData:(dataPokemon,sortBy,sortOrder)=>{
 
 
 
- /*En esta seccion estoy extrayendo datos especificos de la data*/
+ /En esta seccion estoy extrayendo datos especificos de la data/
   /*
  const datapokemon =POKEMON.pokemon;
  
@@ -84,97 +84,4 @@ sortData:(dataPokemon,sortBy,sortOrder)=>{
    let debilidades =elemento.weaknesses;
    imprimir(imagen ,nombre2,id,candy,altura,peso,debilidades);
    alert(imprimir);
- });*/    
- 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-
-
-/*funcion de calcular*/
-/*computeStats:(data) =>{
-  const todo= array ;
-}*/
-
-
-  /*En esta Seccion estoy trabajando en la seccion de busqueda*/
-  /*
-  
-  function searching() {
-    document.getElementById("result").innerHTML = '';
-    let filtro = [
-      "Dragonair",
-      "pikachu",
-      "mew",
-      "mewtwo",
-      "moltres"
-    ];
-    let elemento = document.getElementById("busqueda").value;
-    let proceso = elemento.length;
-    for (indice in filtro) {
-      let name = filtro[indice];
-      let str = name.substring(0, proceso);
-      if (elemento.length <= name.length && elemento.length != 0 && name.length != 0) {
-        if (elemento.toLowerCase() == str.toLowerCase()) {
-          let node = document.createElement("LI");
-          let textnode = document.createTextNode(filtro[indice]);
-          node.appendChild(textnode);
-          document.getElementById("result").appendChild(node);
-        } else {*/
-          /*alert('no')*/
-       /* }*/
-        
-
-
-
-
-  
-  
-  /*
-  
-  let mostrar = (imagen, nombre2, id, candy, altura, peso, debilidades) => {
-   
-      let nombrePokemon = `<p> <img src="${imagen}">
-      <br>Nombre:${nombre2}</br>
-      <br> Su Id es :${id}</br>
-      <br> Su candy es :${candy}</br>
-      <br> Su altura es :${altura}</br>
-      <br> Su peso es :${peso}</br>
-      <br> debilidades :${debilidades}</br>
-      </p>`;
-    }
-  datapokes.forEach(elemento => {
-    let imagen = elemento.img;
-    let nombre2 = elemento.name;
-    let id = elemento.id;
-    let candy = elemento.candy;
-    let altura = elemento.height;
-    let peso =elemento.weight;
-    let debilidades =elemento.weaknesses;
-    mostrar(imagen ,nombre2,id,candy,altura,peso,debilidades);
-   
-    
-  });*/
- 
-  
+ });*/
