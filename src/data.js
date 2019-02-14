9739ed8bface83791c00113f23e0c874948ffe41
eve2921 <eve2921@hotmail.com>
2019-02-14 15:35:43 -0600

    El menu funciona de manera responsive, se coloco el sub menu,se cambio color a negro de del menu, filtra por roles, las tarjetas ahora son cuadradas sin margin top en la imagen

--- src/data.js
+++ src/data.js
@@ -2,24 +2,48 @@ window.lol = {
   iterarData: (datalol) => {
     let datosArr = []
     for (const key in datalol) {
-   if (datalol.hasOwnProperty(key)) {
-     const element = datalol[key];
-     let obj = {
-       name: element.name,
-       tag: element.tags[0],
-       splash: element.splash,
-       title: element.title,
-     }
-     datosArr.push(obj)
+      if (datalol.hasOwnProperty(key)) {
+        const element = datalol[key];
+        let obj = {
+          name: element.name,
+          tag: element.tags,
+          splash: element.splash,
+          title: element.title,
+          difficulty: element.info.difficulty,
+        }
+        datosArr.push(obj)
+      }
+
     }
-   }
-   return datosArr
+    return datosArr
   },
- 
- filtroDataRoles: (rolId, datosArr ) =>{  
-      filtradoPorRoles = datosArr.filter(element => element.tag === rolId);
- 
-       return filtradoPorRoles
-     },
+
+  filtroDataRoles: (rolId, datosArr) => {
+    const filtradoPorRoles = datosArr.filter(element => element.tag[0] === rolId || element.tag[1] === rolId);
+
+    return filtradoPorRoles
+  },
+
+  filterBySearch: (name, datosArr) => {
+    const concidence = datosArr.filter(element => (element.name.toLowerCase().match(name.toLowerCase())));
+    return concidence;
+  },
+
+  sortByDifficulty: (datosArr) => {
+    const queFuncione = datosArr.sort ((a, b) => {
+    if(a.difficulty > b.difficulty){
+      return 1;
+    }
+    if(a.difficulty< b.difficulty){
+      return -1;
+    }
+    else{
+    return 0;
+  } 
+})
+return queFuncione}
   
- }
+
+
+
+}
--- src/main.js
+++ src/main.js
@@ -1,26 +1,42 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});*/
 /*convierte la base de datos en una variable*/
-const datalol = LOL.data;
+const datalol = window.LOL.data;
+const datosArr = window.lol.iterarData(datalol);
+const home = document.getElementById("home");
+const searchChampion = document.getElementById("searchChampion");
 const rol = document.getElementsByClassName('rol');
 const imprimirRoles = document.getElementById("imprimirRoles")  /*trae el div imprimir roles para identificarlo e imprimir*/
-const mostrar = document.getElementById("mostrar")
+let search = document.getElementById("search");
+const difficultyDecent = document.getElementById("difficultyDecent");
 
+home.addEventListener("click",() =>{
+  imprimirRoles.innerHTML = "";
+});
 
+//Función para limpiar contenido antes mostrado y desplegar todos los campeones para realizar busqueda específica 
+searchChampion.addEventListener("click",() =>{
+  imprimirRoles.innerHTML = "";
+  print(datosArr)
+});
+
+//función para pintar los campeones acorde a las propiedades seleccionadas
 const print = (datosArr) =>{
     imprimirRoles.innerHTML = "";
     datosArr.forEach(champ => {
-let pintar = `<div class="cuadro">
-<img class="imagen" src="${champ.splash}"> 
-<div id="subtitulo"><h2 id="nombre">${champ.name}</h2> <h1 id"titulo"> ${champ.title}</h1></div></div>`; 
+let pintar = `<div class="cuadro"><img class="imagen" src="${champ.splash}"> <div id="subtitulo"><h1 id="nombre">${champ.name}</h1> <h2 id"titulo"> ${champ.title}</h2></div></div>`; 
 imprimirRoles.insertAdjacentHTML("beforeend", pintar);
  })
+ 
 
-}
-
+};
+//Función para desplegar los campeones filtrados por roles
 const selectRol = () =>{
     for(let i= 0; i<rol.length;i++){
     rol[i].addEventListener("click",() => {
       let rolId = rol[i].id;
-      const datosArr = window.lol.iterarData(datalol);
       //const printSection = document.getElementById("imprimirRoles");
       const arrayRolesFiltered = window.lol.filtroDataRoles(rolId, datosArr);
       print(arrayRolesFiltered);
@@ -28,3 +44,30 @@ const selectRol = () =>{
   }
 }
 selectRol();
+
+//Función para limpiar contenido antes mostrado y desplegar todos los campeones para realizar busqueda específica 
+// const printChamp = ()=> {
+//     imprimirRoles.innerHTML = "";
+//     print(datosArr)
+// }
+ 
+//Función para desplegar el campeon para se desea buscar
+const filterRolBySearch = () => {
+  search.addEventListener("keyup", () => {
+    let searchValue = search.value;
+    //console.log(searchValue);
+    const showSearch = window.lol.filterBySearch(searchValue, datosArr);
+    print (showSearch);
+  })
+};
+filterRolBySearch ();
+
+const orderByDifficultyDecent = () => {
+  difficultyDecent.addEventListener("click", () => {
+    //console.log("si funciono wey");
+    const decent = window.lol.sortByDifficulty(datosArr);
+    print (decent);
+
+  })};
+  orderByDifficultyDecent();
+  
