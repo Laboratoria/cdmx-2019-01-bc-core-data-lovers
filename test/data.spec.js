9df8eca63ab9eea893e2c04b730063ed5936014b
Ariadna Gutierrez <ariadnagtzr@gmail.com>
2019-02-13 22:42:34 -0600

    cambios en test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,12 @@
-require('../src/data.js');
-
-
-describe('dataFunction', () => {
+let dataCountries = require('../src/data.js');
+let data = require('../src/data.js');
+let countries = require('../src/data.js');
+describe('WorldBank.dataCountries', () => {
   it('is a function', () => {
-    expect(typeof filter).toBe('function');
+    expect(typeof Array).toBe('{[]}');
   });
 
-  it('returns `dataFunction`', () => {
-    expect(example()).toBe('dataFunction');
+  it('returns `dataByCountries`', ([]) => {
+    expect(data[countries].indicators).toBe('dataByCountries');
   });
 });
