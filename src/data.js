1d8394ee19e50d981e620e5c316bfa400aa8a764
J.Fabiola Juárez Z <jefajuza@hotmail.com>
2019-02-15 14:09:47 -0600

    Ya esta implementada la funcion de ordenar por nombre asc y desc y funciona al 100

--- src/data.js
+++ src/data.js
@@ -1,59 +1,75 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-
-//esto a esta funcionando al 100
-window.data={ 
+//esto ya esta funcionando al 100
+window.data = {
 
   //funcion de filtrar
-  
-  
-  
-  filterData:(dataPokemon,types) =>{
-  const arrayPokemon= dataPokemon ;
+
+  filterData: (dataPokemon, types) => {
+    const arrayPokemon = dataPokemon;
+
     const arrayFilter = arrayPokemon.filter(arrayPokemon => arrayPokemon.type.includes(types));
+
     return arrayFilter;
-  }
-  
-  }
-  /*
-  //funcion de ordenar
-  sortData:(dataPokemon,sortBy,sortOrder)=>{
-    let orderDta = [];
-    switch (sortBy){
-      case"name":
-      if (sortOrder === "asc"){
-        orderDta=dataPokemon.sort((a,b) => (a.name>b.name)?1:-1);
-      }
-      else if (sortOrder === "desc"){
-        console.log(sortBy);
-          orderDta= dataPokemon.sort((a,b) => (a.name>b.name)?-1:1);
-      }
-      break;
-      case "width":
-      if (sortOrder === "asc"){
-        orderDta=dataPokemon.sort((a,b) => (a.width>b.name)?1:-1);
-      }else if (sortOrder === "desc"){
-          orderDta= dataPokemon.sort((a,b) => (a.width>b.name)?-1:1);
-      }break
-    
+
+  },
+   sortData: (result, sortBy, sortOrder) => {
+    //data.map(element =>{
+      //element.name = parseFloat(element.name);
+
+    //});
+    if (sortOrder == 'asc'){
+      return result.sort((a,b)=>(a[sortBy]>b[sortBy])?1:-1);
     }
-    sortData(dataPokemon,sortBy,sortOrder);
-  }*/
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-   /*En esta seccion estoy extrayendo datos especificos de la data*/
+    else if (sortOrder == 'desc'){
+      return result.sort((a,b)=>(a[sortBy]<b[sortBy])?1:-1);
+    }
+
+   
+    
+  }
+
+
     /*
+    
+    let orderDta = [];
+    switch (sortBy) {
+      case "name":
+        if (sortOrder === 'asc-name') {
+          orderDta = result.sort((a, b) => (a.name > b.name) ? 1 : -1);
+          console.log(orderDta
+            );
+        } else if (sortOrder === "desc-name") {
+          console.log(sortBy);
+          orderDta = result.sort((a, b) => (a.name > b.name) ? -1 : 1);
+        }
+        break;
+        
+  
+  
+    }
+    sortData(result, sortBy, sortOrder);
+    
+}*/
+   
+
+
+
+//funcion de ordenar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*En esta seccion estoy extrayendo datos especificos de la data*/
+/*
    const datapokemon =POKEMON.pokemon;
    
     
@@ -83,3 +99,95 @@ window.data={
      imprimir(imagen ,nombre2,id,candy,altura,peso,debilidades);
      alert(imprimir);
    });*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*funcion de calcular*/
+/*computeStats:(data) =>{
+  const todo= array ;
+}*/
+
+
+/*En esta Seccion estoy trabajando en la seccion de busqueda*/
+/*
+    
+function searching() {
+  document.getElementById("result").innerHTML = '';
+  let filtro = [
+    "Dragonair",
+    "pikachu",
+    "mew",
+    "mewtwo",
+    "moltres"
+  ];
+  let elemento = document.getElementById("busqueda").value;
+  let proceso = elemento.length;
+  for (indice in filtro) {
+    let name = filtro[indice];
+    let str = name.substring(0, proceso);
+    if (elemento.length <= name.length && elemento.length != 0 && name.length != 0) {
+      if (elemento.toLowerCase() == str.toLowerCase()) {
+        let node = document.createElement("LI");
+        let textnode = document.createTextNode(filtro[indice]);
+        node.appendChild(textnode);
+        document.getElementById("result").appendChild(node);
+      } else {*/
+/*alert('no')*
+/* }*/
+
+
+
+
+
+
+
+/*
+    
+let mostrar = (imagen, nombre2, id, candy, altura, peso, debilidades) => {
+ 
+    let nombrePokemon = `<p> <img src="${imagen}">
+    <br>Nombre:${nombre2}</br>
+    <br> Su Id es :${id}</br>
+    <br> Su candy es :${candy}</br>
+    <br> Su altura es :${altura}</br>
+    <br> Su peso es :${peso}</br>
+    <br> debilidades :${debilidades}</br>
+    </p>`;
+  }
+datapokes.forEach(elemento => {
+  let imagen = elemento.img;
+  let nombre2 = elemento.name;
+  let id = elemento.id;
+  let candy = elemento.candy;
+  let altura = elemento.height;
+  let peso =elemento.weight;
+  let debilidades =elemento.weaknesses;
+  mostrar(imagen ,nombre2,id,candy,altura,peso,debilidades);
+ 
+  
+});*/
+}
