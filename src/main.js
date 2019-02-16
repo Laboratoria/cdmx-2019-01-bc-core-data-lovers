8cbef5c7e1f87f850e153a4026c993258c4c1191
Champy13 <hgftdrd@hotmail.com>
2019-02-16 12:50:37 -0600

    botones y div donde se imprime pokemon responsive

--- src/main.js
+++ src/main.js
@@ -1,53 +1,12 @@
-document.getElementById("next").addEventListener("click", ()=>{
-
-    document.getElementById("navs").style.display="none";
-    document.getElementById("serch").style.display="block";
-    document.getElementById("main").style.display="none";
-
-    imprimir(dataPokemon);
-  })
-
-  document.getElementById("neews").addEventListener("click", ()=>{
-
-      document.getElementById("navs").style.display="block";
-document.getElementById("pokemons").style.display="none";
-document.getElementById("pokemones").style.display="block";
-
-    })
-
-    // document.getElementsById("Fire").addEventListener("click", ()=>{
-    //
-    //     document.getElementById("pokemons").style.display="none";
-    //     document.getElementById("pokemones").style.display="block";
-    //   })
-
-  function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-  // var i = 0;
-  // var pokemonName = dataPokemon.name;
-  // var speed = 50; /* The speed/duration of the effect in milliseconds */
-  //
-  // function typeWriter(dataPokemon) {
-  //   if (i < pokemonName) {
-  //     document.getElementById("nameCenter").innerHTML += txt.charAt(i);
-  //     i++;
-  //     setTimeout(typeWriter, speed);
-  //   }
-  // }
-
-
- var POKEMON;
+document.getElementById("next").addEventListener("click", () => {
+  document.getElementById("serch").style.display = "block";
+  document.getElementById("main").style.display = "none";
+  imprimir(dataPokemon);
+})
+var POKEMON;
 const dataPokemon = POKEMON.pokemon;
 const buttonTypes = Array.from(document.getElementsByClassName("bottonPokemons"));
 const buttonOrder = Array.from(document.getElementsByClassName("sortTipo"));
-
 
 
 
@@ -77,15 +36,9 @@ const sortButton = (botonesOrder) => {
       const idDivi = event.target.id.split("-");
       const sortBy = idDivi[0];
       const sortOrder = idDivi[1];
-
       // const arrayOrde = window.data.sortDataPokemon(dataPokemon, sortBy, sortOrder);
 
       imprimir(window.data.sortDataPokemon(dataPokemon, sortBy, sortOrder));
-
-      const arrayOrde = window.data.sortDataPokemon(dataPokemon, sortBy, sortOrder);
-
-      imprimir(arrayOrde);
-
 
     })
   })
@@ -110,21 +63,14 @@ const imprimir = (obtenerTipo) => {
 
   obtenerTipo.map((dataPokemon) => {
 
-     const pokemons = document.getElementById("pokemons");
-     const pokemones= document.getElementById("pokemones");
-      pokemons.innerHTML="";
-      pokemones.innerHTML="";
-
-
-
-
-
-   pokemons.innerHTML+= `<div class="divPokemon" "><figure class="imgPokemon"><img  src="${dataPokemon.img}"></figure>
-
-                       <p class="namePokemon" id="nameCenter"> Nombre:${dataPokemon.name}</p><br><p class="namePokemon"> ${dataPokemon.type}</p></div>`;
-
-
+    pokemons.innerHTML += `<div class="print-pokemon" ">
+                          <p class="namePokemon numberSite"><i class="fas fa-hashtag"></i> ${dataPokemon.num} / 151</p>
+                          <figure class="imgPokemon"><img src="${dataPokemon.img}"></figure>
+                          <p class="namePokemon"><i class="fas fa-address-card"></i> Nombre: ${dataPokemon.name}</p>                       
+                          <p class="namePokemon"><i class="fas fa-thumbtack"></i> Tipo: ${dataPokemon.type}</p>
+                                                 
+                          </div>`;
+    //<p class="namePokemon"><i class="fas fa-arrows-alt-v"></i> Altura: ${dataPokemon.height}</p>                       
+    // <p class="namePokemon"><i class="fas fa-balance-scale"></i> Peso: ${dataPokemon.weight}</p>
   })
 }
-
-
