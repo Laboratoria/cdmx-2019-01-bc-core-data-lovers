69b9b6578b2b279214df66f52abcc4479a55c719
AdrianaPi <psic.adriana.a@gmail.com>
2019-02-17 15:25:29 -0600

    Se agregaron comentarios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var WORLDBANK; //variable que nos permite validar el test de object
+var WORLDBANK; //variable que nos permite validar el test 
 const dataWorldbank = WORLDBANK; //variable que nos permite enlazar la data y utlizarla
 const buttonCountries = document.getElementsByClassName("btnCountry");
 const selectIndicator = document.getElementById("indicator");
@@ -6,26 +6,25 @@ const dataYear = document.getElementById("data-year");
 
 let dataFilter = {}
 
-//Con esta función enlazamos mediante el id del botón,la data de cada país. Para eso fue necesario poner
-//una misma clase a todos los botones y escribir el ID igual que como está en  la data.
-const buttonClick = () => {
+//Constante para cachar el evento y puntualizar mediante id, el resultado de la función
+const buttonClick = () => { 
   let countries = event.target.id;
   let result = window.WorldBank.dataCountries(dataWorldbank, countries);
   printOption(result);
 }
 
 for (let i = 0; i < buttonCountries.length; i++) {
-  buttonCountries[i].addEventListener("click", buttonClick);
+  buttonCountries[i].addEventListener("click", buttonClick); //Agregar evento a cada uno de los botones
 }
 
-const printOption = (result) => {
-  selectIndicator.innerHTML = "";
+const printOption = (result) => { // Filtrar los indicadores del select por palabra "educación"
+  selectIndicator.innerHTML = ""; //Evita la acumulación de los indicadores cada vez que se da click
   dataYear.innerHTML = "";
   dataFilter = result.filter(indicator => {
     let printIndicatorName = indicator.indicatorName;
-    let education = /educación/i
+    let education = /educación/i; // Se usa la expresión regular -educación- para incorporar los indicadores al select
     let matchs = printIndicatorName.match(education);
-    if (matchs !== null) {
+    if (matchs !== null) {  // Retirar indicadores con valores vacíos
       let indicatorOption = `<option value="${printIndicatorName}" >${printIndicatorName}</option>`
       selectIndicator.insertAdjacentHTML("beforeend", indicatorOption);
       return matchs
@@ -34,7 +33,7 @@ const printOption = (result) => {
 
 }
 
-const printYears = (resultYear) => {
+const printYears = (resultYear) => { { // Imprimir los datos(año-porcentaje) del indicador-original-prueba
   dataYear.innerHTML = "";
   for (const key in resultYear) {
     if (resultYear.hasOwnProperty(key)) {
@@ -89,3 +88,4 @@ const printPercents = (resultYear) => {
 }
 
 selectIndicator.addEventListener("change", sortByPercent);
+
