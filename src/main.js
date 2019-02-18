43b76a942189b05fcaa4624c72ed1ce32d1ba0f7
claudiaramirez <claudiaivonne_15@hotmail.com>
2019-02-18 11:24:46 -0600

    Falta menu, acerca de 1.1

--- src/main.js
+++ src/main.js
@@ -69,17 +69,32 @@ const avg = document.getElementById("avg");
 listQuestion.addEventListener("change", () => {
   indicator.innerHTML = "";//Limpiar funcion
   let country = listQuestion.dataset.ciudad;// Obtenemos la ciudad de la que vamos a filtrar, es decir, obtenemos el data-ciudad del select
+  let QuestionText = listQuestion.options[listQuestion.selectedIndex].text;
   let countrySelect = listQuestion.value;
   const resultado = window.WorldBank.filterCountry(data, country,countrySelect)//Datos de data.js
   for (let resultYear in resultado) { //declaramos una variable y el obejto de donse encuentra lo que vamos a filtrar
     indicator.insertAdjacentHTML('beforeend', `<p><b>Año</b>: ${resultYear} => ${resultado[resultYear] || "N/A"}</p>`);
+   // onbtemos datos para grafica
+    
+   var years = []; // para almacenar los años
+   var dataPerYear = []; // para almacenar el dato por cada año
+    // recorremos el dataciudad
+     years.push(resultYear); // anadimos al final del array years (para eso es push() ) el valor del año que tenemos en la posicion 1..n
+     dataPerYear.push(resultado[resultYear].toString()); // añadimos al final de array los datos reales por año
+  
+   // llamos a la funcion de graficar (par1 eje x par2 eje y, par3 titulo de la grafica)
+   graficar(years, dataPerYear, QuestionText)
+  
+  
   }
-  //codigo prueba 
+  //Promediar
   //let mediaContent = document.createElement('span');
   const media = window.WorldBank.getMathMedia(resultado);
   avg.innerHTML = `Media porcentual:${media}`;
   //indicator.appendChild(mediaContent);
   
+
+ 
 });
 
 const radioFilters = Array.from(document.getElementsByClassName('radio__filter'));
@@ -100,6 +115,31 @@ for (let radioItem in radioFilters){
     }
 
   })
+}
+
+function graficar(datosx, datosy, leyenda){
+  // aqui graficamos
+  //intanciamos chart y como parametro (elemento donde se va a colocar la grafica)
+  var myLineChart = new Chart(graficaDatos, {
+    type: 'line', // graficar linear
+    data:{ // data general
+      labels: datosx, // ["1960","1961"] asi es la estructura
+      datasets: [{ 
+        data: datosy, //  ["14.1", "14.4"] asi es la estructura
+        label: 'Total: ', // etiqueta que aparece al hover del cursor en cada punto de la grafica
+        borderColor: "#ff6182", // color del borde de la linea, en color salmon
+        fill: false, // si la grafica esta rellena desde los picos hasta el eje x
+      }]
+      
+    },
+    options: { // opciones de la grafica
+      title: { // titulo de la grafica
+          display: true, // si se despliega o no
+          text: leyenda // le pasamos el texto de la pregunta que seleccionamos en el select
+      }
+    }
+  }); 
+
 }
 
 // const avg = document.getElementById("avg");
