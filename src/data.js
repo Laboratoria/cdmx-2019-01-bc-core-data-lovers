779531dea561b2f92a9aaacb3ae0d521ed4fd9a9
lore0223 <loremma23@gmail.com>
2019-02-18 19:01:11 -0600

    implementé la indentación de la página en html main data y css

--- src/data.js
+++ src/data.js
@@ -1,53 +1,28 @@
-
 window.pokesaurius = {
-  
+
   typeFilter: (data, pokemonElegido) => {
-   const filterPokemonType = data.filter(element => element.type.includes(pokemonElegido));
-   return filterPokemonType
+    const filterPokemonType = data.filter(element => element.type.includes(pokemonElegido));
+    return filterPokemonType
 
   },
 
 
-  sortByName:(data)=>{
-    const orderPokemonNAme=data.sort((a,b)=> {
-      if(a.name > b.name){
+  sortByName: (data) => {
+    const orderPokemonNAme = data.sort((a, b) => {
+      if (a.name > b.name) {
         return 1;
-      }else{
-        return-1;
+      } else {
+        return -1;
       }
     })
-    return(orderPokemonNAme)
+    return (orderPokemonNAme)
   },
 
 
-//   // getPokemonById:(data, getIdElement) =>{
-//     const pokemon = data.filter(element => element.id == getIdElement);
-//     return pokemon;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-//   {
-//   if (element.id === modalSelected){
-//   console.log(element.id);
-
-//   pokemonId = element;
-//   return element;
-// }
-
-
-
-// return pokemonId;
-// }
+  //   // getPokemonById:(data, getIdElement) =>{
+  //     const pokemon = data.filter(element => element.id == getIdElement);
+  //     return pokemon;
+  // }
 
 
 }
@@ -81,27 +56,25 @@ window.pokesaurius = {
 
 
 
-  //typeSort:(data, ordenElegido) => {    
-  //}
-
-  // nameSort: (data) => {
-  //   const sortPokemonName = elementSort.sort();
-  //   console.log(sortPokemonName)
-
-  // }
-
-
-
-
-
-
-
-
-  //promedio : (peso)=>{
-   //for(let i=0; i< peso.length; i++){
-    // const sumWeight=
-   //}
-
+//typeSort:(data, ordenElegido) => {    
 //}
 
+// nameSort: (data) => {
+//   const sortPokemonName = elementSort.sort();
+//   console.log(sortPokemonName)
 
+// }
+
+
+
+
+
+
+
+
+//promedio : (peso)=>{
+//for(let i=0; i< peso.length; i++){
+// const sumWeight=
+//}
+
+//}
