6937456eb380c8575764983b27223da344fdd838
claudiaferrusca <claudia1274_@hotmail.com>
2019-02-18 18:20:06 -0600

    subiendo cambios con responsive

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 window.pokemon = {
   filterPokemons: (dataPokemon, elementPokemon) => {
     return dataPokemon.filter(pokemon => {
-     // let typePokemon = pokemon.type[0]
-      for(let i=0; i<pokemon.type.length; i++){
+      // let typePokemon = pokemon.type[0]
+      for (let i = 0; i < pokemon.type.length; i++) {
         if (pokemon.type[i] === elementPokemon) {
           return true
         }
@@ -13,21 +13,25 @@ window.pokemon = {
 
   },
 
-ordenarPorNombre :  (pokemonArray) => {
-  const sortName =
-  pokemonArray.sort( (prev, next) =>  {
-    if (prev.name > next.name) {
-      return 1;
-    }else if  (prev.name < next.name) {
-        return -1;
-      }else{
-      return 0;
-    }
+  ordenarPorNombre: (pokemonArray) => {
+    const sortName =
+      pokemonArray.sort((prev, next) => {
+        if (prev.name > next.name) {
+          return 1;
+        } else if (prev.name < next.name) {
+          return -1;
+        } else {
+          return 0;
+        }
 
-  });
-  return sortName
+      });
+    return sortName
 
-},
-
-
+  },
+  totalWeightPokemon: (pokemonArray) => {
+    const pokemonTotalWeight = pokemonArray.reduce((total, dataPokemon) => {
+      return total += parseFloat(dataPokemon.weight);
+    }, 0)
+    return pokemonTotalWeight;
+  }
 }
