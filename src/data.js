33b3a58f21b4f524f40124e395cbe58fd98a587e
Da-ani <daniellesarer93@hotmail.com>
2019-02-21 13:11:49 -0600

    "v"

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 window.data = {
 
-filterPokemon:(dataPokemon,types) =>{
+  filterPokemon: (dataPokemon, types) => {
 
     // const arrayPokemon = dataPokemon;
     // const tipoElegido = types;
@@ -9,8 +9,27 @@ filterPokemon:(dataPokemon,types) =>{
 
     return arrayFilter;
 
-}
+  },
 
+  sortDataPokemon: (data, sortBy, sortOrder) => {
+
+
+    if (sortBy == "weight" && sortOrder == "asc") {
+      return data.sort((a, b) => (parseFloat(a.weight) - parseFloat(b.weight)));
+    } else if (sortBy == "weight" && sortOrder == "dsc") {
+      return data.sort((a, b) => (parseFloat(b.weight) - parseFloat(a.weight)));
+    } else if (sortBy == "height" && sortOrder == "asc") {
+      return data.sort((a, b) => (parseFloat(a.height) - parseFloat(b.height)));
+    } else if (sortBy == "height" && sortOrder == "dsc") {
+      return data.sort((a, b) => (parseFloat(b.height) - parseFloat(a.height)));
+    }
+  },
+
+  filterLetterPokemon: (dataPokemon, letterValue) => {
+
+    const searConside = dataPokemon.filter(dataPokemon => (dataPokemon.name.toLowerCase().match(letterValue.toLowerCase())));
+    return searConside;
+  }
 }
 // imprim: (tipo) => {
 //   let arrNew = [];
