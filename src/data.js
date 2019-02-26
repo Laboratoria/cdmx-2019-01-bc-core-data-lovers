dae63ded77b97b3086f6fed5be70705bc6f4cca3
Karen-Mtz <dkareen.mtz@gmail.com>
2019-02-26 04:36:25 -0600

    -Style.css: nuevo diseño basado en pokedex. -Main.js:  se usó el archivo json para obtener la data. -Data.js: nueva función filtrar por nombre. -Index.html: nueva estructura con header y divs

--- src/data.js
+++ src/data.js
@@ -1,15 +1,23 @@
 window.allPokemon = {
 
   dataFiltered: (data, types) => {
-
     const arrayFilter = data.filter(data => data.type.includes(types));
     return arrayFilter;
 
   },
 
-  orderData: (arrayOfType) => {
+  filterByName: (data, inputValue) => {
+    let searchedByName = data.filter(data => (data.name.toUpperCase().match(inputValue)));
+    return (searchedByName)
+  },
 
+  orderData: (arrayOfType) => {
     arrayOfType.sort((a, b) => a.name > b.name ? 1 : - 1);
+    return arrayOfType;
+  },
+
+  orderDataZtoA: (arrayOfType) => {
+    arrayOfType.sort((a, b) => a.name < b.name ? 1 : - 1);
     return arrayOfType;
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,70 +1,33 @@
-//Declarar objeto de la data
-const data = window.POKEMON.pokemon;
+//VARIABLES
+const url = "./data/pokemon/pokemon.json";
+const buttonFilter = Array.from(document.getElementsByClassName("btn"));
+const sortAtoZ = document.getElementById('bottom-button-sort-AtoZ');
+const sortZtoA = document.getElementById('bottom-button-sort-ZtoA');
+const searcher = document.getElementById('searcher');
+let resultTotal;
+let dataPoke;
 
-// Declara botones
-const startPokedex = document.getElementById('startPokemon');
-const next = document.getElementById('nextPage');
-const back = document.getElementById('regresar');
-const sortt = document.getElementById('ordenar');
-const sortDesk = document.getElementById('ordenarDesktop');
+//Getting data from Json
+fetch(url)
+  .then(response => response.json())
+  .then(data => dataPoke = data.pokemon)
+  .then(() => window.allPokemon.dataFiltered(dataPoke))
+  .then(() => window.allPokemon.filterByName(dataPoke));
 
-
-// Declara Sections
-const start = document.getElementById('start');
-const rootContainer = document.getElementById('rootContainer');
-const options = document.getElementById('options');
-const Charmander = document.getElementById('charmander');
-const gif = document.getElementById('charmanderbackground');
-
-// Declara boton de tipos de pokemones en un array, por medio de su clase.
-const buttonFilter = Array.from(document.getElementsByClassName("boton-typeChart"));
-
-/*
-Cambiar de página en Mobile
-*/
-
-startPokedex.addEventListener("click", () => {
-  start.style.display = "none";
-  rootContainer.style.display = "none";
-  options.style.display = "block";
-});
-
-next.addEventListener("click", () => {
-  start.style.display = "none";
-  rootContainer.style.display = "block";
-  options.style.display = "none";
-});
-
-back.addEventListener("click", () => {
-  start.style.display = "none";
-  rootContainer.style.display = "none";
-  options.style.display = "block";
-});
-
-
-// Obtiene los datos solicitados del objeto Data. Lo compara con el array de botones y filta por tipo.
-
+// FUNCTIONS
 const gettingType = (arrayofButtons) => {
   arrayofButtons.map((buttonSelected) => {
     buttonSelected.addEventListener("click", (event) => {
-      Charmander.style.display = "none";
-      gif.style.display='none';
-      const buttonType = event.target.id;
-      const dataFiltered = window.allPokemon.dataFiltered(data, buttonType);
+      const buttonType = event.target.id
+      const dataFiltered = window.allPokemon.dataFiltered(dataPoke, buttonType)
       printResult(dataFiltered)
     })
-  });
+  })
 };
 
-gettingType(buttonFilter);
-//guarda los resultados en un array
-
-let resultTotal;
-
-// Imprime resultados por tipo. 
 const printResult = (getType) => {
   resultTotal = getType
-  const finalResult = document.getElementById('root');
+  const finalResult = document.getElementById('results');
   finalResult.innerHTML = "";
   getType.map(data => {
     finalResult.innerHTML += `<button class="pokedex"  style='width:100%; height:100%'>
@@ -76,25 +39,23 @@ const printResult = (getType) => {
   return getType;
 };
 
-//ordena de la a - z 
-sortt.addEventListener('click' , () => {
+//INVOCACIONES Y EVENTOS
+
+gettingType(buttonFilter);
+
+searcher.addEventListener('keyup', () => {
+  let inputText = searcher.value
+  let nameAdded = inputText.toUpperCase()
+  let filteredByName = window.allPokemon.filterByName(dataPoke, nameAdded)
+  printResult(filteredByName)
+});
+
+sortAtoZ.addEventListener('click', () => {
   let orderAtoZ = window.allPokemon.orderData(resultTotal);
-  printOrder(orderAtoZ);
+  printResult(orderAtoZ);
 })
-sortDesk.addEventListener('click' , () => {
-  let orderAtoZ = window.allPokemon.orderData(resultTotal);
-  printOrder(orderAtoZ);
-})
-//imprime de la a - z
-const printOrder = (arrayOfTypes) => {
-  const orderResult = document.getElementById('root');
-  orderResult.innerHTML = "";
-  arrayOfTypes.map(data => {
-    orderResult.innerHTML += `<button class="pokedex"  style='width:100%; height:100%'>
-    <img src="${data.img}">
-    <br>Nùmero: ${data.id} 
-    <br> Nombre: ${data.name} 
-    <br> Tipo: ${data.type}</button>`;
-  });
-  return arrayOfTypes;
-};
+
+sortZtoA.addEventListener('click', () => {
+  let orderZtoA = window.allPokemon.orderDataZtoA(resultTotal);
+  printResult(orderZtoA);
+});
